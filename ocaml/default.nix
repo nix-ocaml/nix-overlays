4526c2d96d53098baf9b4a731b693113a02a9a9a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-01-06 22:40:28 -0800

    Override lwt_ppx
    while https://github.com/NixOS/nixpkgs/pull/77182 is pending review

--- ocaml/default.nix
+++ ocaml/default.nix
@@ -54,6 +54,16 @@ in
       };
     });
 
+    lwt_ppx = osuper.lwt_ppx.overrideAttrs (o: {
+      pname = "lwt_ppx";
+      version = "1.2.4";
+
+      src = fetchzip {
+        url = "https://github.com/ocsigen/lwt/archive/4.4.0.tar.gz";
+        sha256 = "1l97zdcql7y13fhaq0m9n9xvxf712jg0w70r72fvv6j49xm4nlhi";
+      };
+    });
+
     camlzip = osuper.camlzip.overrideAttrs (o: {
       buildFlags = if stdenv.hostPlatform != stdenv.buildPlatform then
         "all zip.cmxa"
