1f1db286dde99590412a05539f9536bec7983b8c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-02-01 16:42:02 -0800

    Update Archi

--- ocaml/archi/default.nix
+++ ocaml/archi/default.nix
@@ -7,8 +7,8 @@ let buildArchi = args: buildDunePackage ({
   src = fetchFromGitHub {
     owner = "anmonteiro";
     repo = "archi";
-    rev = "57055b6e40c8528a26941d8c62efe40c591c9118";
-    sha256 = "1j6mqkq8pih2sqjh1i1kqzmg9117m6n62700qhacyxfxapngn9xz";
+    rev = "8610617263dfdb9bc5dc5792ebdd0a7e3f8750ba";
+    sha256 = "0jbqscm3qya5612arppqwkybxm3fbh0had3525ng0raj9paw4578";
   };
 
 } // args);
