d201d074d289a3fbd24f08403482672b45d96378
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-02-17 21:00:40 -0800

    patch magic-mime

--- ocaml/magic-mime/default.nix
+++ ocaml/magic-mime/default.nix
@@ -10,4 +10,7 @@ buildDune2Package (rec {
     url = "https://github.com/mirage/ocaml-magic-mime/releases/download/v${version}/magic-mime-v${version}.tbz";
     sha256 = "1sq4rfd9m3693cnzlbds4qh1xpvrv1iz1s1f75nvacbmfjy0nn8c";
   };
+  postInstall = ''
+    rm $OCAMLFIND_DESTDIR/${pname}/dune-package
+  '';
 })
