545cf71d715cf4a49ff4a06bb94c9abb731e2e14
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-03-08 15:38:43 -0700

    Add sources.nix with pinned nixpkgs

---
+++ sources.nix
@@ -0,0 +1,7 @@
+# `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
+
+import (builtins.fetchTarball {
+  name = "nixos-unstable-2020-03-08";
+  url = https://github.com/nixos/nixpkgs/archive/d008a34.tar.gz;
+  sha256 = "082918aj1gsm581s8qqq9fjm20qzihxygplp6jy07asip3rni54q";
+}) { overlays = [ (import ./.) ]; }
