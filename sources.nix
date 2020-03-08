b89a2872afaefeacb5e35b216e1c2c211337914c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-03-08 15:47:37 -0700

    Don't assume overlays

--- sources.nix
+++ sources.nix
@@ -1,7 +1,9 @@
+{ overlays ? [ (import ./.) ] }:
+
 # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
 
 import (builtins.fetchTarball {
   name = "nixos-unstable-2020-03-08";
   url = https://github.com/nixos/nixpkgs/archive/d008a34.tar.gz;
   sha256 = "082918aj1gsm581s8qqq9fjm20qzihxygplp6jy07asip3rni54q";
-}) { overlays = [ (import ./.) ]; }
+}) { inherit overlays; }
