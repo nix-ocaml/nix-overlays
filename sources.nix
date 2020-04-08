5099dd3117e669e906ee0220d2c6b6a346b81f62
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-08 08:51:47 -0700

    Update NixOS unstable

--- sources.nix
+++ sources.nix
@@ -4,6 +4,6 @@ args@{ overlays ? [ (import ./.) ], ... }:
 
 import (builtins.fetchTarball {
   name = "nixos-unstable-2020-03-28";
-  url = https://github.com/nixos/nixpkgs/archive/ae6bdcc53584aaf20211ce1814bea97ece08a248.tar.gz;
-  sha256 = "0hjhznns1cxgl3hww2d5si6vhy36pnm53hms9h338v6r633dcy77";
+  url = https://github.com/nixos/nixpkgs/archive/39247f8d04c04b3ee629a1f85aeedd582bf41cac.tar.gz;
+  sha256 = "1q7asvk73w7287d2ghgya2hnvn01szh65n8xczk4x2b169c5rfv0";
 }) (args // { inherit overlays; })
