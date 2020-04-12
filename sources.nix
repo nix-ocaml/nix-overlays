a5e37067777b1d981634215a43ed64c2b29f6d3f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-12 11:00:02 -0700

    upgrade nixos unstable

--- sources.nix
+++ sources.nix
@@ -3,7 +3,7 @@ args@{ overlays ? [ (import ./.) ], ... }:
 # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
 
 import (builtins.fetchTarball {
-  name = "nixos-unstable-2020-03-28";
-  url = https://github.com/nixos/nixpkgs/archive/39247f8d04c04b3ee629a1f85aeedd582bf41cac.tar.gz;
-  sha256 = "1q7asvk73w7287d2ghgya2hnvn01szh65n8xczk4x2b169c5rfv0";
+  name = "nixos-unstable-2020-04-10";
+  url = https://github.com/nixos/nixpkgs/archive/807ca93fadd5197c2260490de0c76e500562dc05.tar.gz;
+  sha256 = "10yq8bnls77fh3pk5chkkb1sv5lbdgyk1rr2v9xn71rr1k2x563p";
 }) (args // { inherit overlays; })
