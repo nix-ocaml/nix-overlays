964a4cb04082cdddf93bdadd64d61dbfead8df9d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-17 10:56:48 -0700

    pick up newer unstable sources

--- sources.nix
+++ sources.nix
@@ -3,7 +3,7 @@ args@{ overlays ? [ (import ./.) ], ... }:
 # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
 
 import (builtins.fetchTarball {
-  name = "nixos-unstable-2020-05-15";
-  url = https://github.com/nixos/nixpkgs/archive/32b8ed738096bafb4cdb7f70347a0f63f9f40151.tar.gz;
-  sha256 = "04x5lqw689mfkxzrb53393ssdx2bma8wv0i3hrfsp3d291b2qapx";
+  name = "nixos-unstable-2020-05-16";
+  url = https://github.com/nixos/nixpkgs/archive/b47873026c7e356a340d0e1de7789d4e8428ac66.tar.gz;
+  sha256 = "0wlhlmghfdvqqw2k7nyiiz4p9762aqbb2q88p6sswmlv499x5hb3";
 }) (args // { inherit overlays; })
