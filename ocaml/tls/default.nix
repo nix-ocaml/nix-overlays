cd81f24f57cb5ecf7b576c1c5c28a2b7ada48f09
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-16 18:11:11 -0700

    wip

--- ocaml/tls/default.nix
+++ ocaml/tls/default.nix
@@ -3,11 +3,26 @@
 with oself;
 
 {
+  hkdf = osuper.hkdf.overrideAttrs (o: {
+    postInstall = ''
+      rm $OCAMLFIND_DESTDIR/hkdf/dune-package
+    '';
+  });
+
+  tls = osuper.tls.overrideAttrs (o: {
+    postInstall = ''
+      rm $OCAMLFIND_DESTDIR/tls/dune-package
+    '';
+  });
+
   tls-mirage = buildDunePackage {
     pname = "tls-mirage";
     version = osuper.tls.version;
     src = osuper.tls.src;
     useDune2 = true;
+    postInstall = ''
+      rm $OCAMLFIND_DESTDIR/tls-mirage/dune-package
+    '';
     propagatedBuildInputs = [
       tls
       x509
