d074ab743d887724e51264f83c99d11c917a115f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-30 09:19:26 -0700

    pg_query: don't depend on Core

--- ocaml/pg_query/default.nix
+++ ocaml/pg_query/default.nix
@@ -6,20 +6,22 @@ buildDunePackage {
   pname = "pg_query";
   version = "0.9.4-dev";
 
-  src = fetchFromGitHub {
-    owner = "roddyyaga";
-    repo = "pg_query-ocaml";
-    rev = "bae4c23d4a62f0b0559ed5a8552bf481574250de";
-    sha256 = "1bx4zbnpyi0994r59xl91rw2iak1lmhqq8178i333wj4snkflk5w";
+  unpackPhase = ''
+    runHook preUnpack
+    tar -xzf $src --strip-components=1 --exclude='bin'
+    runHook postUnpack
+  '';
+
+  src = builtins.fetchurl {
+    url = http://github.com/roddyyaga/pg_query-ocaml/archive/bae4c23d4a62f0b0559ed5a8552bf481574250de.tar.gz;
+    sha256 = "11vlpv924w8bq7mcimzgwchpfvn39ij6mpzmh2vnqyghmmayxb9z";
   };
 
   useDune2 = true;
 
   propagatedBuildInputs = with ocamlPackages; [
-    core
     ppx_inline_test
     ppx_deriving
-    ppx_jane
     ctypes
   ];
 }
