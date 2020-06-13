773fb247431747355681bff21849ffed8605f2a0
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-06-13 10:42:20 -0700

    hash

--- sources.nix
+++ sources.nix
@@ -5,6 +5,6 @@ args@{ overlays ? [ (import ./.) ], ... }:
 import (builtins.fetchTarball {
   name = "nixos-unstable-2020-06-06";
   url = https://github.com/nixos/nixpkgs/archive/029a5de08390bb03c3f44230b064fd1850c6658a.tar.gz;
-  sha256 = "156rj5kch114radmni79rm6pg7g1i10wrpm1ghvf0fwns7jfrm3j";
+  sha256 = "03fjkzhrs2avcvdabgm7a65rnyjaqbqdnv4q86qyjkkwg64g5m8x";
 }) (args // { inherit overlays; })
 
