f6addaf1786d54676be4c2f126aa80de6423711a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-07-11 13:28:49 -0700

    jose

--- ocaml/jose/default.nix
+++ ocaml/jose/default.nix
@@ -7,8 +7,8 @@ buildDunePackage rec {
   version = "0.4.0";
   src = builtins.fetchurl {
     # WIP version, doesn't support AES256 fully
-    url = "https://github.com/ulrikstrid/reason-jose/archive/cc3b292daea1069de334879765d0c914a706f21d.tar.gz";
-    sha256 = "1hpykm0rxiy2q909v6jsdw352zkmmyflxis8m5kwpqzfa1w8kjz2";
+    url = https://github.com/ulrikstrid/reason-jose/archive/69ac466b37252b1f263468a02eebf599724a9b84.tar.gz;
+    sha256 = "0vh95a59c08vfzdsfmzihm6niqclnp84yxy9iip6mc20vf77zgph";
   };
 
   propagatedBuildInputs = [
