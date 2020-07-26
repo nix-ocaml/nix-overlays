a889410f42373c3cc0f665779eff647490f2532a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-07-25 20:54:12 -0700

    udpate jose to 0.5.0

--- ocaml/jose/default.nix
+++ ocaml/jose/default.nix
@@ -4,10 +4,10 @@ with ocamlPackages;
 
 buildDunePackage rec {
   pname = "jose";
-  version = "0.4.0";
+  version = "0.5.0";
   src = builtins.fetchurl {
-    url = https://github.com/ulrikstrid/reason-jose/archive/f4a9d1a484c1f4335210e4d9320b65d6a8594f45.tar.gz;
-    sha256 = "0d251f75birpylmykrhpkznca0jqdz7qs6ckv4q253xcqk23lncr";
+    url = https://github.com/ulrikstrid/reason-jose/releases/download/v0.5.0/jose-v0.5.0.tbz;
+    sha256 = "03mr39rm6splhsyrlhp9rmzmh1b8nk3c8kf8n61ww389nc2866yf";
   };
 
   propagatedBuildInputs = [
