42def7161d07a7feef3d50720657f3bdf17dfc3a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-08-03 20:22:57 -0700

    new gluten iteration

--- ocaml/gluten/default.nix
+++ ocaml/gluten/default.nix
@@ -6,20 +6,20 @@ let
   buildGluten = args: buildDunePackage (rec {
     version = "0.2.1";
     src = builtins.fetchurl {
-      url = https://github.com/anmonteiro/gluten/archive/d3540d576f54aba1e41480aa89d4cdbad4167604.tar.gz;
-      sha256 = "1vlxh02gb0amsap8xp65m6zzn3hgp35w4xg0qiz369bxd6mndqz3";
+      url = https://github.com/anmonteiro/gluten/archive/7b2bbc5111f5a2b25150f9219af5481587b0f47c.tar.gz;
+      sha256 = "0dwnkaxhhi75wjxrnb55bl0hnjgh53wwr41im68d04rggni5zjrg";
     };
   } // args);
 
 in {
     gluten = buildGluten {
       pname = "gluten";
-      propagatedBuildInputs = [ faraday ];
+      propagatedBuildInputs = [ bigstringaf faraday ke ];
     };
 
     gluten-lwt = buildGluten {
       pname = "gluten-lwt";
-      propagatedBuildInputs = [ gluten lwt ke ];
+      propagatedBuildInputs = [ gluten lwt ];
     };
 
     gluten-lwt-unix = buildGluten {
