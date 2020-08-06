59a1195bf8f566eb964ca99b29c079c628177d31
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-08-06 09:51:29 -0700

    update nix sources

--- sources.nix
+++ sources.nix
@@ -4,7 +4,7 @@ args@{ overlays ? [ (import ./.) ], ... }:
 
 import (builtins.fetchTarball {
   name = "nixos-unstable-2020-07-30";
-  url = https://github.com/nixos/nixpkgs/archive/840c782d507d60aaa49aa9e3f6d0b0e780912742.tar.gz;
-  sha256 = "14q3kvnmgz19pgwyq52gxx0cs90ddf24pnplmq33pdddbb6c51zn";
+  url = https://github.com/nixos/nixpkgs/archive/8e2b14aceb1d40c7e8b84c03a7c78955359872bb.tar.gz;
+  sha256 = "0zzjpd9smr7rxzrdf6raw9kbj42fbvafxb5bz36lcxgv290pgsm8";
 }) (args // { inherit overlays; })
 
