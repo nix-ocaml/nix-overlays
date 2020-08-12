1e7c7be4e3d3dc1ae912f63362e0f389121f1987
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-08-11 20:40:34 -0700

    update sources

--- sources.nix
+++ sources.nix
@@ -3,8 +3,8 @@ args@{ overlays ? [ (import ./.) ], ... }:
 # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
 
 import (builtins.fetchTarball {
-  name = "nixos-unstable-2020-08-08";
-  url = https://github.com/nixos/nixpkgs/archive/b50ef9afa11b384c72f7123ca4760ebc6d199fe8.tar.gz;
-  sha256 = "0b6xjysxydiax765sia3dpnc3xi648aq4zjlxpiqzsh3hpsq0ch8";
+  name = "nixos-unstable-2020-08-11";
+  url = https://github.com/nixos/nixpkgs/archive/32b46dd897ab2143a609988a04d87452f0bbef59.tar.gz;
+  sha256 = "1gzfrpjnr1bz9zljsyg3a4zrhk8r927sz761mrgcg56dwinkhpjk";
 }) (args // { inherit overlays; })
 
