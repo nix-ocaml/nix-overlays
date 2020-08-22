2a0d08cd0a8c7e81aa478a3cebe7e2fe1d159383
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-08-22 14:31:23 -0700

    graphql_ppx

--- ocaml/graphql_ppx/default.nix
+++ ocaml/graphql_ppx/default.nix
@@ -7,8 +7,8 @@ buildDunePackage rec {
   version = "1.0.0-beta.22";
 
   src = builtins.fetchurl {
-    url = https://github.com/reasonml-community/graphql-ppx/archive/v1.0.0-beta.22.tar.gz;
-    sha256 = "1597gvksdcz725b0l0n2wnhnjjmw70qbkir3mdfrf153v5xkg7fx";
+    url = https://github.com/reasonml-community/graphql-ppx/archive/468fad5c55866006329918fb1c06f9695d0a13bd.tar.gz;
+    sha256 = "0d3dnzv9ws2mj85l07mnn2ib3hkr2668y9bwscv6sypi4xgd6f03";
   };
 
   useDune2 = true;
