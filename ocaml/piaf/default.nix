ddc8290430e3ebea398debec0ceeaa9a73f0783d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-08-22 22:15:43 -0700

    update piaf

--- ocaml/piaf/default.nix
+++ ocaml/piaf/default.nix
@@ -3,8 +3,8 @@
 with ocamlPackages;
 
 let src = builtins.fetchurl {
-  url = https://github.com/anmonteiro/piaf/archive/eba8d7a007c276d8d34a88b1752e8e6c5eec1c9a.tar.gz;
-  sha256 = "17xagjbm3vxbsjkbpl3xzn40kxnb8mgmzv63sl3a8xpqixrlmz79";
+  url = https://github.com/anmonteiro/piaf/archive/fc22870f1d9b7a25b6e71ae56234cbf4999d1d13.tar.gz;
+  sha256 = "0dawpcry348cm4mfpn605zxbp8kb0gvqlirfc8bqf5d10pd12kyq";
 };
 
 in
