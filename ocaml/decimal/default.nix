afdd1d2640be7dabb6e697b798a60f3a22ac8908
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-12-01 20:15:02 -0800

    update decimal

--- ocaml/decimal/default.nix
+++ ocaml/decimal/default.nix
@@ -4,10 +4,10 @@ with ocamlPackages;
 
 buildDunePackage rec {
   pname = "decimal";
-  version = "0.0.3";
+  version = "0.2.0";
   src = builtins.fetchurl {
     url = "https://github.com/yawaramin/ocaml-decimal/releases/download/v${version}/decimal-v${version}.tbz";
-    sha256 = "100ydshdfhr0lqglbr0y79i6h6v1s2f47w1qlcyb6m3x21lwi6nq";
+    sha256 = "19khc7d4fj7i98nbilniz42912ifwzw49vxb5diy9yzrr2s8wxpg";
   };
 
   propagatedBuildInputs = [ zarith ];
