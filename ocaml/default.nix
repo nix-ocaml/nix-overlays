01b5d45d144929db9742cdf666c19a06871b26dd
Ulrik <ulrik.strid@outlook.com>
2021-01-19 22:49:27 +0100

    Update reenv to latest master

--- ocaml/default.nix
+++ ocaml/default.nix
@@ -417,13 +417,26 @@ in
 
     reenv = osuper.buildDunePackage {
       pname = "reenv";
-      version = "0.3.1-dev";
+      version = "0.4.0-dev";
       src = builtins.fetchurl {
-        url = https://github.com/ulrikstrid/reenv/archive/57f5c418264840ecb6114f5afa3be0f0c8c14943.tar.gz;
-        sha256 = "0lh72r5wxkmdx5228bqm3g70khafsfk8i65imnlyhcab7gjdc8vn";
+        url = https://github.com/ulrikstrid/reenv/archive/64dbee58dbe01c86e24db3bdcb8961fdc178deb1.tar.gz;
+        sha256 = "1bv8rxnjnydfrvc55xbywsl98hq664khg5agacyk7ba0r1vlblh6";
       };
 
+      buildInputs = [
+        alcotest
+        junit
+        junit_alcotest
+      ];
+
+      doCheck = true;
+
       propagatedBuildInputs = [reason cmdliner re];
+
+      meta = {
+        description = "dotenv-cli written in reason";
+        license = stdenv.lib.licenses.mit;
+      };
     };
 
     rosetta = callPackage ./rosetta { ocamlPackages = oself; };
