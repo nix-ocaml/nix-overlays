5400b8857537a0c0f57f776a8c0efb37e5c632bb
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-01-19 12:37:02 -0800

    ignore lablgtk

--- ci.nix
+++ ci.nix
@@ -4,17 +4,17 @@ let
   pkgs = import ./sources.nix {};
   inherit (pkgs) lib stdenv;
   ignoredPackages = [
-    "bap" "bin_prot_p4" "bolt" "camlp4" "camomile_0_8_2" "cil"
-    "dypgen" "earlybird" "enumerate" "estring" "faillib" "fieldslib_p4" "gtktop"
-    "lablgtk_2_14" "lens" "magick" "mezzo" "mlgmp" "ocaml_cairo" "ocaml_cryptgps"
-    "ocaml_data_notation" "ocaml_http" "ocamlnat" "ocamlsdl" "ocf" "omake_rc1"
-    "pa_ounit" "ppx_deriving_protobuf" "sodium" "sqlite3EZ" "type_conv_108_08_00"
-    "type_conv_109_60_01" "variantslib_p4" "xtmpl" "async_inotify" "async_smtp"
-    "lwt_camlp4" "js_of_ocaml-camlp4" "macaque" "odn" "comparelib" "config-file"
-    "erm_xmpp" "gmetadom" "herelib" "higlo" "js_build_tools" "lablgtk-extras"
-    "lua-ml" "ocaml_optcomp" "ocamlscript" "ocsigen_deriving" "pa_bench" "pipebang"
-    "rope" "stog" "type_conv" "type_conv_112_01_01" "typerep" "typerep_p4" "ulex"
-    "lablgl"
+    "bap" "bin_prot_p4" "bolt" "camlp4" "camomile_0_8_2" "cil" "dypgen"
+    "earlybird" "enumerate" "estring" "faillib" "fieldslib_p4" "gtktop"
+    "lablgtk_2_14" "lablgtk" "lablgtk-extras" "lens" "magick" "mezzo" "mlgmp"
+    "ocaml_cairo" "ocaml_cryptgps" "ocaml_data_notation" "ocaml_http"
+    "ocamlnat" "ocamlsdl" "ocf" "omake_rc1" "pa_ounit" "ppx_deriving_protobuf"
+    "sodium" "sqlite3EZ" "type_conv_108_08_00" "type_conv_109_60_01"
+    "variantslib_p4" "xtmpl" "async_inotify" "async_smtp" "lwt_camlp4"
+    "js_of_ocaml-camlp4" "macaque" "odn" "comparelib" "config-file" "erm_xmpp"
+    "gmetadom" "herelib" "higlo" "js_build_tools" "lua-ml" "ocaml_optcomp"
+    "ocamlscript" "ocsigen_deriving" "pa_bench" "pipebang" "rope" "stog"
+    "type_conv" "type_conv_112_01_01" "typerep" "typerep_p4" "ulex" "lablgl"
 
     "cairo2"
   ];
