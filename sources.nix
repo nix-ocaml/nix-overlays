013c7723742f051bed4d69fc32cf30ed4c0da3a9
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-01-21 07:47:22 -0800

    update sources to pick up new ppx_tools

--- sources.nix
+++ sources.nix
@@ -3,8 +3,8 @@ args@{ overlays ? [ (import ./.) ], ... }:
 # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
 
 import (builtins.fetchTarball {
-  name = "nixos-unstable-2021-01-18";
-  url = https://github.com/nixos/nixpkgs/archive/68398d2dd50efc2d878bf0f83bbc8bc323b6b0e0.tar.gz;
-  sha256 = "1bivcxnajll53ixwyl304fq22w5dg97fqbwk8imp6ipwq84bq5ga";
+  name = "nixos-unstable-2021-01-21";
+  url = https://github.com/nixos/nixpkgs/archive/33fc9b1c6dec3287425ad88e60e6534e33d1f9b9.tar.gz;
+  sha256 = "1pvfrbp5qlwiv20b35hc74mfpvgi08q6naz1gwlldpxdmi6173gd";
 }) (args // { inherit overlays; })
 
