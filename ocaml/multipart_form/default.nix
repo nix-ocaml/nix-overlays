8f375b55ada7cf8aaf4e982698063c547e5e5e6e
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-01-23 15:43:01 -0800

    multipart_form

--- ocaml/multipart_form/default.nix
+++ ocaml/multipart_form/default.nix
@@ -5,16 +5,21 @@ with ocamlPackages;
 buildDunePackage {
   pname = "multipart_form";
 
-  version = "0.0.1-dev";
+  version = "0.1.0-dev";
 
   src = builtins.fetchurl {
-    url = https://github.com/dinosaure/multipart_form/archive/c53bc1ddad2784d3069bf82aaa4370d762a1a870.tar.gz;
-    sha256 = "0xwl0rr5vigvsm40pc0ilx7av329b8xqw34srbpj2y6g1spjgs65";
+    url = https://github.com/anmonteiro/multipart_form/archive/6ad738e423d53c53ab494d6e6526f42140db1098.tar.gz;
+    sha256 = "1w2bzpvsc1i6ifi9jmcgz2f8shnfgdmrkzh1jj3mj2dlqcgpipxx";
   };
+
+  doCheck = true;
+  checkInputs = [ alcotest ];
 
   propagatedBuildInputs = [
     astring
     base64
+    mrmime
+    faraday
     pecu
     rosetta
     rresult
