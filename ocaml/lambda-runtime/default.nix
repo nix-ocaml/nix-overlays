c6b1df8dc82523e76b7a554037a416e975e77ba8
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-01-31 15:56:43 -0800

    fix lambda-runtime packages

--- ocaml/lambda-runtime/default.nix
+++ ocaml/lambda-runtime/default.nix
@@ -6,8 +6,8 @@ let
   buildLambdaRuntime = args: buildDunePackage ({
     version = "0.1.0-dev";
     src = builtins.fetchurl {
-      url = https://github.com/anmonteiro/aws-lambda-ocaml-runtime/archive/1fe828601e0a7c3f2c97e5490f1b5db5bface553.tar.gz;
-      sha256 = "03lxjcd7qx6v7dqjp6j83xzfn56vdhaxak8g6k3515lm87vx24vc";
+      url = https://github.com/anmonteiro/aws-lambda-ocaml-runtime/archive/af70a42a527fdbcdb3eaab6776a997333a6becb4.tar.gz;
+      sha256 = "14rrv4rdkn1q5yb1pdh2l2pqhx7lnx38wbgkbzd0hayalcn3d6d8";
     };
   } // args);
 
@@ -17,11 +17,11 @@ in {
     propagatedBuildInputs = [ yojson ppx_deriving_yojson piaf uri logs lwt ];
   };
 
-  now = buildLambdaRuntime {
-    pname = "now";
+  vercel = buildLambdaRuntime {
+    pname = "vercel";
     propagatedBuildInputs = [
       lambda-runtime
-      httpaf
+      piaf
       yojson
       ppx_deriving_yojson
       lwt
