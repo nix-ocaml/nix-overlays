f2ac24bda2ab84bb71ba8edfdc6cf1aed109fcae
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-02-03 21:10:33 -0800

    menhir

--- ocaml/menhir/default.nix
+++ ocaml/menhir/default.nix
@@ -3,10 +3,10 @@
 with ocamlPackages;
 let
   buildMenhir = args: buildDunePackage (rec {
-    version = "20201201";
+    version = "20201216";
     src = builtins.fetchurl {
       url = "https://gitlab.inria.fr/fpottier/menhir/repository/${version}/archive.tar.gz";
-      sha256 = "1nw8l1rr4wila795g9cg5d3sakb7d3mldgp186r1iyjy74hfwxjb";
+      sha256 = "05fpg5c83a6q0q12kd2ll069pg80yd91s4rzx3742ard3l2aml8z";
     };
     useDune2 = true;
   } // args);
