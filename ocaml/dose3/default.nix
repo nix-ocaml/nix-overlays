c30fbc367027702e529eee50942e96203806b247
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-02-11 10:05:11 -0800

    Fix dose3 (#36)

--- ocaml/dose3/default.nix
+++ ocaml/dose3/default.nix
@@ -1,32 +1,25 @@
-{ stdenv, ocamlPackages, perl }:
+{ bzip2, ocamlPackages }:
 
 with ocamlPackages;
 
-stdenv.mkDerivation {
-  name = "dose";
+buildDunePackage {
+  pname = "dose3";
+  version = "6.1";
   src = builtins.fetchurl {
-    url = "http://gforge.inria.fr/frs/download.php/file/36063/dose3-5.0.1.tar.gz";
-    sha256 = "00yvyfm4j423zqndvgc1ycnmiffaa2l9ab40cyg23pf51qmzk2jm";
+    url = https://gitlab.com/irill/dose3/-/archive/6.1/dose3-6.1.tar.gz;
+    sha256 = "0cqdk3gdf4sxyv073s3xmgcp78n03la7mpahvn41hw339z3kcjgb";
   };
-  buildInputs = [
-    ocaml
-    findlib
-    ocamlbuild
-    cppo
-    perl
-  ];
+
   propagatedBuildInputs = [
-    cudf
+    bzip2
     ocaml_extlib
+    base64
+    camlbz2
+    camlzip
+    cudf
     ocamlgraph
     re
-  ];
-  createFindlibDestdir = true;
-  patches = [
-    ./patches/0001-Install-mli-cmx-etc.patch
-    ./patches/0002-dont-make-printconf.patch
-    ./patches/0003-Fix-for-ocaml-4.06.patch
-    ./patches/0004-Add-unix-as-dependency-to-dose3.common-in-META.in.patch
-    ./patches/dose.diff
+    parmap
+    stdlib-shims
   ];
 }
