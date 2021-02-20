04f1e91534bb5c0a8383331930f033ab3e255e94
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-02-20 12:23:03 -0800

    new sources to fix a package (#44)

--- sources.nix
+++ sources.nix
@@ -4,8 +4,8 @@ args@{ overlays ? [ (import ./.) ], ... }:
 
 import
   (builtins.fetchTarball {
-    name = "nixos-unstable-2021-02-14";
-    url = https://github.com/nixos/nixpkgs/archive/6b1057b452c55bb3b463f0d7055bc4ec3fd1f381.tar.gz;
-    sha256 = "10qfg11g8m0q2k3ibcm0ivjq494gqynshm3smjl1rfn5ifjf5fz8";
+    name = "nixos-unstable-2021-02-20";
+    url = https://github.com/nixos/nixpkgs/archive/9816b99e71c3504b0b4c1f8b2e004148460029d4.tar.gz;
+    sha256 = "1dpz36i3vx0c1wmacrki0wsf30if8xq3bnj71g89rsbxyi87lhcm";
   })
   (args // { inherit overlays; })
