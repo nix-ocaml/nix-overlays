2d5e062aa8b9a7576ad44d295a12ba055295423d
Ulrik <ulrik.strid@outlook.com>
2021-03-08 21:17:12 +0100

    Use correct sha

--- sources.nix
+++ sources.nix
@@ -6,6 +6,6 @@ import
   (builtins.fetchTarball {
     name = "nixos-unstable-2021-03-07";
     url = https://github.com/nixos/nixpkgs/archive/1005b14672de.tar.gz;
-    sha256 = "0dljrdm9icdid8zx1y1lmlldl2i0xrin6fqjb1nwn0nbdmwqshws";
+    sha256 = "1815gsqh6md3k5416g1hpk4x2718py2wg5qagkd8avpm8y149k5l";
   })
   (args // { inherit overlays; })
