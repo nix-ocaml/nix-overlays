22d5c10cfc4fbdff8c8b80bba1fc44f66d71197b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-03-09 11:28:06 -0800

    even newer

--- sources.nix
+++ sources.nix
@@ -4,8 +4,8 @@ args@{ overlays ? [ (import ./.) ], ... }:
 
 import
   (builtins.fetchTarball {
-    name = "nixos-unstable-2021-03-08";
-    url = https://github.com/nixos/nixpkgs/archive/1005b14672de.tar.gz;
-    sha256 = "1815gsqh6md3k5416g1hpk4x2718py2wg5qagkd8avpm8y149k5l";
+    name = "nixos-unstable-2021-03-09";
+    url = https://github.com/nixos/nixpkgs/archive/0867f6274247.tar.gz;
+    sha256 = "09ajwk2aafhy2xiqlcp5swlhhfiwdjb3dbfm8p8j81h28vli7p94";
   })
   (args // { inherit overlays; })
