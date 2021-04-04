022ba5b3d5b2dfbd606b25467ef5afa738d2268d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-04-04 15:37:57 -0700

    newer sources, update ppx_jsx_embed

--- ocaml/ppx_jsx_embed/default.nix
+++ ocaml/ppx_jsx_embed/default.nix
@@ -6,8 +6,8 @@ buildDunePackage {
   pname = "ppx_jsx_embed";
   version = "0.0.0";
   src = builtins.fetchurl {
-    url = https://github.com/melange-re/ppx_jsx_embed/archive/2aa5e28.tar.gz;
-    sha256 = "007y6xg1c0ivqhxv8r1ndnnr24savaw5gkg9z1q5ycfp8i0zjagb";
+    url = https://github.com/melange-re/ppx_jsx_embed/archive/888ffc77.tar.gz;
+    sha256 = "122r9y301a32nm33j3qzh6lzglapsc1nkw4a2cwym8i35d2s8v09";
   };
   doCheck = true;
   useDune2 = true;
