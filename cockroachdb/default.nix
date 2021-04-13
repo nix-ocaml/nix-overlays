99df0f1361a842ad797d219ffe71861c3f89b896
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-04-13 12:36:49 -0700

    update cockroach db (#67)

--- cockroachdb/default.nix
+++ cockroachdb/default.nix
@@ -5,21 +5,21 @@ with pkgs;
 {
   cockroachdb-20_x = callPackage ./generic.nix (rec {
     go = go_1_15;
-    version = "20.2.5";
+    version = "20.2.7";
     src = builtins.fetchurl
       {
         url = "https://binaries.cockroachdb.com/cockroach-v${version}.src.tgz";
-        sha256 = "181h0ywk6r661fy1z37jbdhxz8hn4q09n795l9063cqndm8gcr7r";
+        sha256 = "11jh7fyyp9acnp8z4vc38rc2391sjl5d4aij9pf61wl7bfaqyb79";
       };
   });
 
   cockroachdb-21_x = callPackage ./generic.nix (rec {
     go = go_1_15;
-    version = "21.1.0-alpha.3";
+    version = "21.1.0-beta.3";
     src = builtins.fetchurl
       {
         url = "https://binaries.cockroachdb.com/cockroach-v${version}.src.tgz";
-        sha256 = "1jfn53lyapqsi9zm4khplfqp7lkja2s7gk78vcgz8zfaqi551qq7";
+        sha256 = "1sx22a7ax7pxf6q4vcnjh6x5v5d85lq409axl4xwml7iiqln18mr";
       };
   });
 }
