6f7c4936d07a78f483e44fd266451da620ffa870
Ulrik Strid <ulrik.strid@outlook.com>
2021-06-29 14:00:40 +0200

    Update data-encoding

--- ocaml/tezos/data-encoding.nix
+++ ocaml/tezos/data-encoding.nix
@@ -2,19 +2,20 @@
 
 let
   src = builtins.fetchurl {
-    url = https://gitlab.com/nomadic-labs/data-encoding/-/archive/0.3/data-encoding-0.3.tar.gz;
-    sha256 = "185i5iyya0d2n3818hd758zrf0pbc234j915lahri355xa6167c7";
+    url = https://gitlab.com/nomadic-labs/data-encoding/-/archive/v0.4/data-encoding-v0.4.tar.gz;
+    sha256 = "1nlg8bm5yh107jbaba5rplr31iz3pafaqqaqsb626cb09z408ddz";
   };
 
 in
 
 ocamlPackages.buildDunePackage {
   pname = "data-encoding";
-  version = "0.3.0";
+  version = "0.4.0";
   inherit src;
 
   buildInputs = with ocamlPackages; [
     alcotest
+    crowbar
   ];
 
   doCheck = true;
@@ -22,9 +23,9 @@ ocamlPackages.buildDunePackage {
   propagatedBuildInputs = with ocamlPackages; [
     ezjsonm
     zarith
+    hex
     json-data-encoding
     json-data-encoding-bson
-    crowbar
   ];
 
   meta = {
