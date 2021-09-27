f114005df59e1da7b608f7943df3f714e69cbffb
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-09-26 19:56:33 -0700

    updated reason hash

--- ocaml/reason/default.nix
+++ ocaml/reason/default.nix
@@ -11,8 +11,8 @@ buildDunePackage {
       builtins.fetchurl
         {
           # https://github.com/reasonml/reason/pull/2657 -- OCaml 4.13 support
-          url = https://github.com/reasonml/reason/archive/b6d8bff0.tar.gz;
-          sha256 = "08jvbaql7a0mpmpwyzndjsl2403wk545nqhw80ba02r5r4wg297p";
+          url = https://github.com/reasonml/reason/archive/8ea317b073.tar.gz;
+          sha256 = "050a6bsx1bwrc73p732kpdydcbhflv1ddg39pnlnzqf90pvmgli1";
         } else
       builtins.fetchurl {
         url = https://registry.npmjs.org/@esy-ocaml/reason/-/reason-3.7.0.tgz;
