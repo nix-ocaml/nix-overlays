d57fd6a82aea2b407a1486a3460e6a0bae5ec5ec
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-10-05 20:08:12 -0700

    new sources, remove logs override

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
-    name = "nixos-unstable-2021-10-01";
-    url = https://github.com/nixos/nixpkgs/archive/82155ff501c.tar.gz;
-    sha256 = "0xv47cpgaxb4j46ggjx9gkg299m9cdfzar27xw5h5k2lg5d3dljg";
+    name = "nixos-unstable-2021-10-04";
+    url = https://github.com/nixos/nixpkgs/archive/f91fec348dd7.tar.gz;
+    sha256 = "0kl6cz81z0jc6kzpwlbn4r2dpn25sfdvq3jykhbrbx43l4hxkz1k";
   };
 
   staging = builtins.fetchTarball {
