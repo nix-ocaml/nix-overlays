4e89153bac5dec0cf6ab653f2eb18ac1968a5c40
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-10-09 11:02:32 -0700

    maybe this one

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
     name = "nixos-unstable-2021-10-08";
-    url = https://github.com/nixos/nixpkgs/archive/9e84e5f8f36e.tar.gz;
-    sha256 = "0qf1227hrn8ddv1ip3mnx8gy2lw3h2ibz82qlr96s5b048x043w9";
+    url = https://github.com/nixos/nixpkgs/archive/70088dc29994.tar.gz;
+    sha256 = "08ldqfh2cmbvf930yq9pv220sv83k9shq183935l5d8p61fxh5zr";
   };
 
   staging = builtins.fetchTarball {
