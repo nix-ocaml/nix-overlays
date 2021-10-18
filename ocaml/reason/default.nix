3cada1095de106a07ad19e5f4f27c978e6f60faa
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-10-17 20:35:27 -0700

    wip

--- ocaml/reason/default.nix
+++ ocaml/reason/default.nix
@@ -6,10 +6,19 @@ buildDunePackage {
   pname = "reason";
   version = "3.7.0";
 
-  src = builtins.fetchurl {
-    url = https://github.com/reasonml/reason/archive/ccc34729994b4a80d4f6274cc0165cd9113444d6.tar.gz;
-    sha256 = "00hy9wpp7qyjs1nzq0hmjywqsz1xb9360icrrdr32pp9j84bym4i";
-  };
+  src =
+    if (lib.versionOlder "4.13" ocaml.version) then
+      builtins.fetchurl
+        {
+          # https://github.com/reasonml/reason/pull/2657 -- OCaml 4.13 support
+          url = https://github.com/reasonml/reason/archive/8ea317b073.tar.gz;
+          sha256 = "050a6bsx1bwrc73p732kpdydcbhflv1ddg39pnlnzqf90pvmgli1";
+        } else
+    # Melange depends on the 4.08 AST, and until that's fixed we use this Reason version
+      builtins.fetchurl {
+        url = https://github.com/reasonml/reason/archive/ccc34729994b4a80d4f6274cc0165cd9113444d6.tar.gz;
+        sha256 = "00hy9wpp7qyjs1nzq0hmjywqsz1xb9360icrrdr32pp9j84bym4i";
+      };
 
   propagatedBuildInputs = [
     menhir
