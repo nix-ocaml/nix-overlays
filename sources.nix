86d0df28e91246cf9d324a598901799dee55bce7
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-10-20 20:05:00 -0700

    fix hash

--- sources.nix
+++ sources.nix
@@ -2,7 +2,7 @@
   unstable = builtins.fetchTarball {
     name = "nixos-unstable-2021-10-16";
     url = https://github.com/nixos/nixpkgs/archive/3ef1d2a9602.tar.gz;
-    sha256 = "0ajd2lzsjxw2zdc35a5xw8ci0k5vizj63xvxnpya2m45wr9dn5rj";
+    sha256 = "0yicccl89rfa5nk4ic46ydihvzsw1phzsypnlzmzrdnwsxi3r9d4";
   };
 
   staging = builtins.fetchTarball {
