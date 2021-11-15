75e66cf495d4d448475185ce2e18338fdd310408
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-11-14 22:19:41 -0800

    more cleanup

--- default.nix
+++ default.nix
@@ -4,47 +4,10 @@ self: super:
 
 let
   inherit (super) lib stdenv fetchFromGitHub callPackage;
-
-  ocamlVersions = [ "4_06" "4_08" "4_09" "4_10" "4_11" "4_12" "4_13" ];
-  ocamlOverlay = callPackage ./ocaml { };
-  overlayOcamlPackages = (extraOverlays:
-    self: super:
-      let
-        overlayOcamlPackages = version: {
-          "ocamlPackages_${version}" =
-            builtins.foldl'
-              (acc: x: acc.overrideScope' x)
-              super.ocaml-ng."ocamlPackages_${version}"
-              extraOverlays;
-        };
-        oPs =
-          lib.fold lib.mergeAttrs { }
-            (builtins.map overlayOcamlPackages ocamlVersions);
-
-      in
-      {
-        ocaml = self.ocamlPackages.ocaml;
-        ocamlPackages = oPs.ocamlPackages_4_12;
-        ocamlPackages_latest = self.ocamlPackages;
-
-        ocaml-ng = super.ocaml-ng // oPs // {
-          ocamlPackages = self.ocamlPackages;
-          ocamlPackages_multicore = (oPs.ocamlPackages_4_12.overrideScope' (oself: osuper: {
-            ocaml = osuper.ocaml.overrideAttrs (_: {
-              version = "4.12.0+multicore+effects";
-              # hardeningDisable = [ "strictoverflow" ];
-              src = builtins.fetchurl {
-                url = https://github.com/ocaml-multicore/ocaml-multicore/archive/9468d8c91feac55c9b390e50c25b739de043e5e0.tar.gz;
-                sha256 = "0zymc0rm5kmydr97jdjbj0wdvlb0ldw57f2i7d915vf8xd0xz9ba";
-              };
-            });
-          })).overrideScope' ocamlOverlay;
-        };
-      }
-  );
+  overlayOcamlPackages = import ./ocaml/overlay-ocaml-packages.nix { inherit lib callPackage; };
 in
-(overlayOcamlPackages [ ocamlOverlay ] self super) //
-{
+
+(overlayOcamlPackages [ (callPackage ./ocaml { }) ] self super) // {
   # Stripped down postgres without the `bin` part, to allow static linking
   # with musl
   libpq = super.postgresql.override { enableSystemd = false; gssSupport = false; };
@@ -53,18 +16,23 @@ in
     inherit (self) ocamlPackages;
   });
 
-  pkgsCross = super.pkgsCross // {
-    musl64 = super.pkgsCross.musl64.appendOverlays (callPackage ./static { });
+  pkgsCross =
+    let
+      static-overlays = callPackage ./static { };
+      cross-overlays = callPackage ./cross { };
+    in
+    super.pkgsCross // {
 
-    aarch64-multiplatform =
-      super.pkgsCross.aarch64-multiplatform.appendOverlays
-        (callPackage ./cross { });
+      musl64 = super.pkgsCross.musl64.appendOverlays static-overlays;
 
-    aarch64-multiplatform-musl =
-      (super.pkgsCross.aarch64-multiplatform-musl.appendOverlays
-        ((callPackage ./cross { }) ++
-        (callPackage ./static { })));
-  };
+      aarch64-multiplatform =
+        super.pkgsCross.aarch64-multiplatform.appendOverlays
+          cross-overlays;
+
+      aarch64-multiplatform-musl =
+        (super.pkgsCross.aarch64-multiplatform-musl.appendOverlays
+          (cross-overlays ++ static-overlays));
+    };
 
   nixUnstable = super.nixUnstable.overrideAttrs (o:
     let
@@ -74,7 +42,6 @@ in
         sha256 = "122wbh8srahcgavkhmqwl9hllyppgra7m6f52yxbiwrznbaxxvbc";
       };
     in
-
     {
       name = "${lib.substring 0 (lib.stringLength o.version - 7) o.version}${lib.substring 0 7 rev}";
       inherit src;
