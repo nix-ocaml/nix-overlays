6d76ddb7c351ffc36a7943e35041433d88f85b90
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-11-14 21:54:49 -0800

    Unify OCaml overlay generation

--- static/default.nix
+++ static/default.nix
@@ -1,4 +1,4 @@
-{ pkgsNative, lib, ocamlVersions }:
+{ pkgsNative, lib, callPackage }:
 # Loosely adapted from https://github.com/serokell/tezos-packaging/blob/b7617f99/nix/static.nix
 
 [
@@ -12,27 +12,5 @@
 
   (import ./overlays.nix)
 
-  (self: super:
-    let
-      overlayOcamlPackages = version: {
-        "ocamlPackages_${version}" =
-          super.ocaml-ng."ocamlPackages_${version}".overrideScope'
-            (super.callPackage ./ocaml.nix { })
-        ;
-      };
-      oPs =
-        lib.fold lib.mergeAttrs { }
-          (builtins.map overlayOcamlPackages ocamlVersions);
-
-    in
-    {
-      ocamlPackages = oPs.ocamlPackages_4_12;
-      ocamlPackages_latest = self.ocamlPackages;
-
-      ocaml-ng = super.ocaml-ng // oPs // {
-        ocamlPackages = self.ocamlPackages;
-        ocamlPackages_latest = self.ocamlPackages;
-      };
-    }
-  )
+  (lib.overlayOcamlPackages [ (callPackage ./ocaml.nix { }) ])
 ]
