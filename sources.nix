1ac14767cd46d4cf9b1dd72a07880dab1d32d90e
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-11-23 17:59:38 -0800

    Update sources

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
     name = "nixos-unstable-2021-11-21";
-    url = https://github.com/nixos/nixpkgs/archive/53edfe1d1c5.tar.gz;
-    sha256 = "0gnliwsj61sfgfkq2wf1llzxrchvi38vq6h8my96g5aznv49vhim";
+    url = https://github.com/nixos/nixpkgs/archive/db22325869a.tar.gz;
+    sha256 = "0pihqkl1c5bmb62657r38irvacav51ab0r4vfa2wn027ch1ry29m";
   };
 
   staging = builtins.fetchTarball {
