af8cdb03491552826087d71bf1cbd56df97b19d4
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-12-24 03:06:22 -0800

    newer

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
-    name = "nixos-unstable-2021-12-19";
-    url = https://github.com/nixos/nixpkgs/archive/988b00665215.tar.gz;
-    sha256 = "0a2kz6lya6jnwds6cvg1x409gciv5jd3lh1xciy3ilzaxal7ski3";
+    name = "nixos-unstable-2021-12-24";
+    url = https://github.com/nixos/nixpkgs/archive/eac07edbd20e.tar.gz;
+    sha256 = "11r1b0kpyls2w4n7s9xkj2kim715vrk6aj7ackia4f8s9imachk6";
   };
 
   staging = builtins.fetchTarball {
