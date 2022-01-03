969a6a21ec9e370c7bf02ca5ef043bc437ce8703
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-01-03 12:13:10 -0800

    fix maybe

--- cockroachdb/default.nix
+++ cockroachdb/default.nix
@@ -1,21 +1,21 @@
-{ callPackage, go_1_15, go_1_16 }:
+{ callPackage, go_1_15, go_1_16, go_1_17 }:
 
 {
   cockroachdb-21_1_x = callPackage ./generic.nix (rec {
     go = go_1_15;
-    version = "21.1.8";
+    version = "21.1.12";
     src = builtins.fetchurl {
       url = "https://binaries.cockroachdb.com/cockroach-v${version}.src.tgz";
-      sha256 = "04i2y8qzhipyrbvyqyjflmqrzzc9vl3ic22h09n777ha95x0r7b7";
+      sha256 = "116zzrwzs6sbkwvnxgs8aidqxhc5a3vrk3acbgdnjbyfxxrrkv8j";
     };
   });
 
   cockroachdb-21_2_x = callPackage ./generic.nix (rec {
-    go = go_1_16;
-    version = "21.2.2";
+    go = go_1_17;
+    version = "21.2.3";
     src = builtins.fetchurl {
-      url = "https://binaries.cockroachdb.com/cockroach-v21.2.2.src.tgz";
-      sha256 = "07qwqlyn6lbq1j6rf58hy0n2hmp46jm2s66qjsisb60g5ydmdb19";
+      url = "https://binaries.cockroachdb.com/cockroach-v${version}.src.tgz";
+      sha256 = "1lyxkwyljg82dd8m5b18zbvli02znj2aq2yqf8bpbrsd1psv3m8w";
     };
     patches = [ ./makefile-redact-safe-patch.patch ];
   });
