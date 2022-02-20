68ebc7e863675c0fc01dfc3901fbc1cdee005f17
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-02-20 03:28:19 -0800

    Build an OCaml 5.00 cross-compiler

--- ocaml/overlay-ocaml-packages.nix
+++ ocaml/overlay-ocaml-packages.nix
@@ -1,5 +1,5 @@
 let
-  ocamlVersions = [ "4_06" "4_08" "4_09" "4_10" "4_11" "4_12" "4_13" ];
+  ocamlVersions = [ "4_06" "4_08" "4_09" "4_10" "4_11" "4_12" "4_13" "5_00" ];
 in
 
 extraOverlays:
@@ -8,11 +8,38 @@ self: super:
 
 let
   inherit (super) lib callPackage;
+
+  ocaml-ng_with_500 =
+    super.ocaml-ng //
+    (if !(super.ocaml-ng ? "ocamlPackages_5_00") then {
+      ocamlPackages_5_00 = super.ocaml-ng.ocamlPackages_4_13.overrideScope' (oself: osuper:
+        let
+          sources = "${import ../sources.nix}/pkgs/development/compilers/ocaml/generic.nix";
+        in
+        {
+          ocaml = (callPackage
+            (import sources {
+              major_version = "5";
+              minor_version = "00";
+              patch_version = "0+trunk";
+            })
+            { }).overrideAttrs
+            (_: {
+              version = "5.00+trunk";
+              hardeningDisable = [ "strictoverflow" ];
+              src = builtins.fetchurl {
+                url = https://github.com/ocaml/ocaml/archive/e7b3839fcd1925473beb4bcf9cdf82c87213dc59.tar.gz;
+                sha256 = "0dkv5yi78qkgr274dcrmddwm3bj1as2iz1nsg375wwy9hsd91gsj";
+              };
+            });
+        });
+    } else { });
+
   overlayOcamlPackages = version: {
     "ocamlPackages_${version}" =
       builtins.foldl'
         (acc: x: acc.overrideScope' x)
-        super.ocaml-ng."ocamlPackages_${version}"
+        ocaml-ng_with_500."ocamlPackages_${version}"
         extraOverlays;
   };
   oPs =
@@ -25,18 +52,7 @@ in
   ocamlPackages = oPs.ocamlPackages_4_12;
   ocamlPackages_latest = self.ocamlPackages;
 
-  ocaml-ng = super.ocaml-ng // oPs // {
+  ocaml-ng = ocaml-ng_with_500 // oPs // {
     ocamlPackages = self.ocamlPackages;
-    ocamlPackages_multicore = (oPs.ocamlPackages_4_12.overrideScope' (oself: osuper: {
-      ocaml = osuper.ocaml.overrideAttrs (_: {
-        version = "5.00+trunk";
-        hardeningDisable = [ "strictoverflow" ];
-        src = builtins.fetchurl {
-          url = https://github.com/ocaml/ocaml/archive/e7b3839fcd1925473beb4bcf9cdf82c87213dc59.tar.gz;
-          sha256 = "0dkv5yi78qkgr274dcrmddwm3bj1as2iz1nsg375wwy9hsd91gsj";
-        };
-      });
-
-    })).overrideScope' (callPackage ./. { });
   };
 }
