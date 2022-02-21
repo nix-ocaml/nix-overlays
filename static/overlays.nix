8a451f2cbbcd3105341ddb757a76c31edf25917b
Ulrik Strid <ulrik.strid@outlook.com>
2022-02-21 15:26:16 +0100

    ...

--- static/overlays.nix
+++ static/overlays.nix
@@ -19,7 +19,7 @@ in
   zlib-oc = super.zlib.override { static = true; splitStaticOutput = false; };
   openssl-oc = super.openssl.override { static = true; };
   gmp-oc = super.gmp.override { withStatic = true; };
-  libffi-oc = super.libffi.override { withStatic = true; };
+  libffi-oc = super.libffi.overrideAttrs (_: { dontDisableStatic = true; });
 }
 # // (
 # lib.mapAttrs' renameForOCaml { inherit (pkgsStatic) zlib openssl; }
