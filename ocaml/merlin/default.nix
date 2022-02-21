2ff332bb1f9136ada061fbf85b0b99b68cfebec4
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-02-20 23:03:15 -0800

    fix merlin and make reason work for 5.00

--- ocaml/merlin/default.nix
+++ ocaml/merlin/default.nix
@@ -1,7 +1,7 @@
 { lib
 , substituteAll
 , ocaml
-, dune_2
+, dune_3
 , buildDunePackage
 , dot-merlin-reader
 , yojson
@@ -9,17 +9,24 @@
 , result
 }:
 
+# let camlp-streams-patch = if (lib.versionOlder "5.00" ocaml.version)a then
+
+# in
 buildDunePackage {
   pname = "merlin";
   inherit (dot-merlin-reader) src version;
 
-  patches = [
-    (substituteAll {
-      src = ./fix-paths.patch;
-      dot_merlin_reader = "${dot-merlin-reader}/bin/dot-merlin-reader";
-      dune = "${dune_2}/bin/dune";
-    })
-  ] ++ lib.optional (lib.versionOlder "4.12" ocaml.version) ./camlp-streams.patch;
+  postPatch = ''
+    substituteInPlace src/kernel/mconfig_dot.ml --replace \
+      'let prog = "dot-merlin-reader"' \
+      'let prog = "${dot-merlin-reader}/bin/dot-merlin-reader"'
+    substituteInPlace src/kernel/mconfig_dot.ml --replace \
+      'let prog = "dune"' \
+      'let prog = "${dune_3}/bin/dune"'
+    substituteInPlace src/frontend/ocamlmerlin/dune --replace \
+      '(libraries ' \
+      '(libraries camlp-streams '
+  '';
 
   buildInputs = [ dot-merlin-reader yojson csexp result ];
 }
