b59a2c4dc6f08b583fc3777c49677577b4e1542b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-03 11:38:37 -0800

    newer sources, check if kube is cached (#199)

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
     name = "nixos-unstable-small-2022-03-02";
-    url = https://github.com/nixos/nixpkgs/archive/d9aa42326fd.tar.gz;
-    sha256 = "0kdms6gdw5w7slncijpccx0mqdg5v89pmw342cxxrdb3v4dyh1gd";
+    url = https://github.com/nixos/nixpkgs/archive/3e072546ea98.tar.gz;
+    sha256 = "1b51j0zz4gfcmq1lzh0f9yj6h904p7fgskshvc70dkjkdg9k2x7j";
   };
 
   staging = builtins.fetchTarball {
