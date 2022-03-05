722de5442088bf10fdad7145baaf67437f0d2dad
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-05 14:21:47 -0800

    Update melange-compiler-libs

--- ocaml/melange/compiler-libs.nix
+++ ocaml/melange/compiler-libs.nix
@@ -5,8 +5,8 @@ buildDunePackage {
   version = "0.0.0";
 
   src = builtins.fetchurl {
-    url = https://github.com/melange-re/melange-compiler-libs/archive/78a33622.tar.gz;
-    sha256 = "015gqgq5b9kdpw0y5hwhcj8wxl72rqh13d3g4w36yp24kxh05c88";
+    url = https://github.com/melange-re/melange-compiler-libs/archive/83e3017.tar.gz;
+    sha256 = "02h0gzhk6bdxy6iarp8lk6yl80cskxiraqmbplpa5nqn7r9h2d3l";
   };
 
   propagatedBuildInputs = [ menhir menhirLib ];
