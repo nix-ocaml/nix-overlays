fde87077261b1329fa07fc088abbaca4ac75477f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-08 17:27:45 -0800

    Unblock a few packages (#209)
    * Unblock a few packages
    
    * Update graphql_ppx and fix reason for 5.00

--- ocaml/reason/default.nix
+++ ocaml/reason/default.nix
@@ -11,22 +11,22 @@
 , result
 }:
 
-buildDunePackage {
+buildDunePackage rec {
   pname = "reason";
-  version = "3.7.0";
+  version = "3.8.0";
 
   src =
-    if (lib.versionOlder "4.13" ocaml.version)
+    if lib.versionOlder "5.00" ocaml.version
     then
       builtins.fetchurl
         {
           url = https://github.com/reasonml/reason/archive/64689cb65951cd30d13b8c63a21ed956834ee26a.tar.gz;
           sha256 = "00whcci10yf7hkbzksakr92zna351229c7cqabp3sm87h4hzfhzc";
-        } else
-    # Melange depends on the 4.08 AST, and until that's fixed we use this Reason version
+        }
+    else
       builtins.fetchurl {
-        url = https://registry.npmjs.org/@esy-ocaml/reason/-/reason-3.7.0.tgz;
-        sha256 = "0spqbsphxnpp3jdy4amfgw58w6mam5gb4vn9gxm5nh9rkcz0iaqg";
+        url = "https://github.com/reasonml/reason/releases/download/${version}/reason-${version}.tbz";
+        sha256 = "0yc94m3ddk599crg33yxvkphxpy54kmdsl599c320wvn055p4y4l";
       };
 
   propagatedBuildInputs = [
