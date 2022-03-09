97bf01abfa0d25ad711d8f4af85f2a056039a947
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-08 16:22:26 -0800

    Update graphql_ppx and fix reason for 5.00

--- ocaml/graphql_ppx/default.nix
+++ ocaml/graphql_ppx/default.nix
@@ -5,8 +5,8 @@ buildDunePackage {
   version = "1.1.0";
 
   src = builtins.fetchurl {
-    url = https://github.com/reasonml-community/graphql-ppx/archive/refs/tags/v1.1.0.tar.gz;
-    sha256 = "1dkkcr5ypphlx96n7h448cwazld9phygvlqr5giqvckqk72qc3pn";
+    url = https://github.com/teamwalnut/graphql-ppx/archive/refs/tags/v1.2.3.tar.gz;
+    sha256 = "0jwaf4rac2c9n4k01hf3krjklflhld0z0wynh32666yav10h4zdb";
   };
 
   useDune2 = true;
--- ocaml/reason/default.nix
+++ ocaml/reason/default.nix
@@ -15,10 +15,19 @@ buildDunePackage rec {
   pname = "reason";
   version = "3.8.0";
 
-  src = builtins.fetchurl {
-    url = "https://github.com/reasonml/reason/releases/download/${version}/reason-${version}.tbz";
-    sha256 = "0yc94m3ddk599crg33yxvkphxpy54kmdsl599c320wvn055p4y4l";
-  };
+  src =
+    if lib.versionOlder "5.00" ocaml.version
+    then
+      builtins.fetchurl
+        {
+          url = https://github.com/reasonml/reason/archive/64689cb65951cd30d13b8c63a21ed956834ee26a.tar.gz;
+          sha256 = "00whcci10yf7hkbzksakr92zna351229c7cqabp3sm87h4hzfhzc";
+        }
+    else
+      builtins.fetchurl {
+        url = "https://github.com/reasonml/reason/releases/download/${version}/reason-${version}.tbz";
+        sha256 = "0yc94m3ddk599crg33yxvkphxpy54kmdsl599c320wvn055p4y4l";
+      };
 
   propagatedBuildInputs = [
     menhir
