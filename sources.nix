bf3340b55f952bc19b032ff0e68ffc0ff0738eda
UpdateBot <ulrikstrid@users.noreply.github.com>
2022-03-09 11:34:54 +0000

    Update sources

--- sources.nix
+++ sources.nix
@@ -1,7 +1,7 @@
 {
   unstable = builtins.fetchTarball {
     name = "nixos-unstable-small-2022-03-07";
-    url = https://github.com/nixos/nixpkgs/archive/35db2e60.tar.gz;
+    url = https://github.com/nixos/nixpkgs/archive/35db2e60df598e3d5c88adab65f70b91f6645b45.tar.gz;
     sha256 = "0hdrvb35bzh4yb2ykbqs44aiqwan02kcikqhsy5ay064pi1wxw2a";
   };
 
