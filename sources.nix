f7de94347b210e406bbe42b39bf56041483fe20d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-10 20:52:40 -0800

    update sources (#215)
    * update sources
    
    * disable for macos
    
    * disable properly
    
    * disable ogg too
    
    * grr

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
-    name = "nixos-unstable-small-2022-03-07";
-    url = https://github.com/nixos/nixpkgs/archive/35db2e60.tar.gz;
-    sha256 = "0hdrvb35bzh4yb2ykbqs44aiqwan02kcikqhsy5ay064pi1wxw2a";
+    name = "nixos-unstable-small-2022-03-10";
+    url = https://github.com/nixos/nixpkgs/archive/085e8934e46f.tar.gz;
+    sha256 = "0bf403mnl2zayc3xr1hawqggsdp66yc08vfp7n15xh5bc5vpz1y8";
   };
 
   staging = builtins.fetchTarball {
