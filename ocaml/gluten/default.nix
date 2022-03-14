a557731b2e850d3c2eb4b305de31dd676a2993b8
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-13 21:42:33 -0700

    update gluten

--- ocaml/gluten/default.nix
+++ ocaml/gluten/default.nix
@@ -5,8 +5,8 @@ buildDunePackage {
   version = "0.2.2-dev";
 
   src = builtins.fetchurl {
-    url = https://github.com/anmonteiro/gluten/archive/c5eada58.tar.gz;
-    sha256 = "1a6dcrwpycrncjxhr76vaz57hxjhnnk7sps5k3xjl7948p9qy14d";
+    url = https://github.com/anmonteiro/gluten/archive/8d31bd7.tar.gz;
+    sha256 = "1fxhb3hpr29avf4fpkgjmgm0i00ypk2lcw47gbbvk7nx3jlhinbg";
   };
 
   propagatedBuildInputs = [ bigstringaf faraday ke ];
