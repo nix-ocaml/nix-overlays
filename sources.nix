c0d6fbf7f7ceccc351e84b27d5584c3948f61633
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-03-23 10:41:00 -0700

    Update sources (#227)
    Co-authored-by: UpdateBot <ulrikstrid@users.noreply.github.com>

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
-    name = "nixos-unstable-small-2022-03-20";
-    url = https://github.com/nixos/nixpkgs/archive/e2f381b2f1879ad6d68be5a9e9e6e3288529bfaf.tar.gz;
-    sha256 = "0h262iqg0v472gc05fzzyhn8fpqb430klgxpl9pmg44kx0bx1a69";
+    name = "nixos-unstable-small-2022-03-22";
+    url = https://github.com/nixos/nixpkgs/archive/5dbd4b2b27e24eaed6a79603875493b15b999d4b.tar.gz;
+    sha256 = "0f6514l8jva85v1g1vvib93691pwr25blzxr4i4vavys5dz6kxnm";
   };
 
   staging = builtins.fetchTarball {
