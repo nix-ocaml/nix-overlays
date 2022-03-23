0682cf2ad13150ba8b12847761ac601d39445ba5
Ulrik Strid <ulrik.strid@outlook.com>
2022-03-23 21:25:40 +0100

    fix dream-deps on 5.00

--- ocaml/tyxml/jsx.nix
+++ ocaml/tyxml/jsx.nix
@@ -4,6 +4,14 @@ buildDunePackage {
   inherit (tyxml) src version;
   pname = "tyxml-jsx";
 
+  postPatch = ''
+    substituteInPlace jsx/tyxml_jsx.ml \
+      --replace "Char.lowercase "  "Char.lowercase_ascii " \
+      --replace "Char.uppercase "  "Char.uppercase_ascii " \
+      --replace "String.lowercase "  "String.lowercase_ascii " \
+      --replace "String.uppercase "  "String.uppercase_ascii "
+  '';
+
   propagatedBuildInputs = [ tyxml tyxml-syntax ppxlib reason ];
 
   checkInputs = [ alcotest ];
--- ocaml/tyxml/ppx.nix
+++ ocaml/tyxml/ppx.nix
@@ -11,6 +11,12 @@
 buildDunePackage {
   pname = "tyxml-ppx";
   inherit (tyxml) src version;
+
+  postPatch = ''
+    substituteInPlace ppx/tyxml_ppx.ml \
+      --replace "String.capitalize "  "String.capitalize_ascii "
+  '';
+
   propagatedBuildInputs = [ tyxml tyxml-syntax ppxlib reason markup ];
 
   checkInputs = [ alcotest ];
