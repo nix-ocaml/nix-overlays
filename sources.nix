7c1214203bc109902b2292673f9ba46bfabf73e5
UpdateBot <ulrikstrid@users.noreply.github.com>
2022-03-30 19:10:15 +0000

    Update sources

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
-    name = "nixos-unstable-2022-03-23";
-    url = https://github.com/nixos/nixpkgs/archive/1d08ea2bd83abef174fb43cbfb8a856b8ef2ce26.tar.gz;
-    sha256 = "1q8p2bz7i620ilnmnnyj9hgx71rd2j6sjza0s0w1wibzr9bx0z05";
+    name = "nixos-unstable-small-2022-03-29";
+    url = https://github.com/nixos/nixpkgs/archive/00e27c78d3d2de6964096ceee8d70e5b487365e3.tar.gz;
+    sha256 = "0xxc4wbf3izdsmy9pw4f3nrdv2d2kwsdm269gkxik27jxlc61n61";
   };
 
   staging = builtins.fetchTarball {
