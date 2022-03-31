3bc18acc5819ecfafeb89a7737782c1e314c6d34
Ulrik Strid <ulrik.strid@outlook.com>
2022-03-31 14:16:07 +0200

    Newer sources

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
-    name = "nixos-unstable-small-2022-03-29";
-    url = https://github.com/nixos/nixpkgs/archive/9a2de8ca73e84455276d84c044791b7dbc3d77e9.tar.gz;
-    sha256 = "0qxw7dzq20z5xnpnnj8v4rir0bmlgqvjxpyzdfas1237hkzjsw7v";
+    name = "nixos-unstable-2022-03-31";
+    url = https://github.com/nixos/nixpkgs/archive/710fed5a2483f945b14f4a58af2cd3676b42d8c8.tar.gz;
+    sha256 = "1xhbkgb9rzh2b0rbyhcygvc6216g9qbqyjkkgrhwfclsx06sfach";
   };
 
   staging = builtins.fetchTarball {
