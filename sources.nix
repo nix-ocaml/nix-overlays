50621af95f9f8679bb8d9f05de02a0c7601cf4d3
UpdateBot <ulrikstrid@users.noreply.github.com>
2022-04-29 11:37:49 +0000

    Update sources

--- sources.nix
+++ sources.nix
@@ -1,8 +1,8 @@
 {
   unstable = builtins.fetchTarball {
-    name = "nixos-unstable-small-2022-04-27";
-    url = https://github.com/nixos/nixpkgs/archive/d6b996030dd21c6509803267584b9aca3e133a07.tar.gz;
-    sha256 = "1zjlnmph80i077cp1ywsfkv9p6y40f6i3dm6g3jc7f3xlq61vv4s";
+    name = "nixos-unstable-small-2022-04-28";
+    url = https://github.com/nixos/nixpkgs/archive/6766fb6503ae1ebebc2a9704c162b2aef351f921.tar.gz;
+    sha256 = "1a805n9iqlbmffkzq3l6yf2xp74wjaz5pdcp0cfl0rhc179w4lpy";
   };
 
   staging = builtins.fetchTarball {
