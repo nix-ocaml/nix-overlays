9376aeaea0a532d50df5fb0fd4202e0e0d1f5c05
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-04-30 15:02:19 -0700

    unbreak CI? (#257)
    * unbreak CI?
    
    * clean up ci.nix
    
    remove packages that were removed ustream too
    
    * morph is dead, long live morph
    
    * remove more exclusions
    
    * upgrade dream-livereload
    
    * async_inotify and async_smtp aren't available for macOS
    
    * build more
    
    * will this repro it?
    
    * does indeed repro

--- ocaml/dream-livereload/default.nix
+++ ocaml/dream-livereload/default.nix
@@ -2,10 +2,10 @@
 
 buildDunePackage rec {
   pname = "dream-livereload";
-  version = "0.1.0";
+  version = "0.2.0";
   src = builtins.fetchurl {
-    url = "https://github.com/tmattio/dream-livereload/archive/refs/tags/${version}.tar.gz";
-    sha256 = "0id3yav4wgn5klx5gdh7mnb91ppa7z80f9279csvgy1qrzca65ja";
+    url = https://github.com/tmattio/dream-livereload/releases/download/0.2.0/dream-livereload-0.2.0.tbz;
+    sha256 = "1ppq4j823p57w7bmzclmlb035i1mhjwz86alwjr44bjv493h6rgr";
   };
 
   propagatedBuildInputs = [ dream lambdasoup lwt_ppx ];
