992d6924930c0af72926e2123125a7c987cec914
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-05-03 20:10:10 -0700

    fix ocamlformat-rpc for real

--- ocaml/ocamlformat-rpc/default.nix
+++ ocaml/ocamlformat-rpc/default.nix
@@ -1,16 +1,20 @@
 { callPackage }:
 
 
-let mkocamlformat-rpc = version: callPackage ./generic.nix {
-  inherit version;
-}; in
+let mkocamlformat-rpc = callPackage ./generic.nix; in
 
 rec {
-  ocamlformat-rpc_0_20_0 = mkocamlformat-rpc "0.20.0";
+  ocamlformat-rpc_0_20_0 = mkocamlformat-rpc {
+    version = "0.20.0";
+    ocamlformat = null;
+  };
 
-  ocamlformat-rpc_0_20_1 = mkocamlformat-rpc "0.20.1";
+  ocamlformat-rpc_0_20_1 = mkocamlformat-rpc {
+    version = "0.20.1";
+    ocamlformat = null;
+  };
 
-  ocamlformat-rpc_0_21_0 = mkocamlformat-rpc "0.21.0";
+  ocamlformat-rpc_0_21_0 = mkocamlformat-rpc { version = "0.21.0"; };
 
   ocamlformat-rpc = ocamlformat-rpc_0_21_0;
 }
