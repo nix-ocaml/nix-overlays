8f520d7844b25caa34be437223a419654b2eb6b0
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-08 22:02:21 +0200

    fmt

--- ci/hydra.nix
+++ ci/hydra.nix
@@ -10,16 +10,16 @@ with filter;
   # build_4_14 = buildCandidates { inherit pkgs; ocamlVersion = "4_14"; };
   build_5_00 = buildCandidates { inherit pkgs; ocamlVersion = "5_00"; extraIgnores = ocaml5Ignores; };
 
-/*
-  arm64_4_13 = (if system == "x86_64-linux" then
+  /*
+    arm64_4_13 = (if system == "x86_64-linux" then
     crossTarget pkgs.pkgsCross.aarch64-multiplatform-musl "4_13"
-  else
+    else
     { });
 
-  musl_4_13 = (if system == "x86_64-linux" then
+    musl_4_13 = (if system == "x86_64-linux" then
     crossTarget pkgs.pkgsCross.musl64 "4_13"
-  else
+    else
     { }
     );
-*/
+  */
 }
