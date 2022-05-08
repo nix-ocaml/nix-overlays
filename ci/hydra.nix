f778939d80985e0d96ad82132aa667b7bbf6f078
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-08 21:24:31 +0200

    Disable some stuff until I get a new ssd

--- ci/hydra.nix
+++ ci/hydra.nix
@@ -5,11 +5,12 @@ in
 
 with filter;
 {
-  build_4_12 = buildCandidates { inherit pkgs; ocamlVersion = "4_12"; };
+  # build_4_12 = buildCandidates { inherit pkgs; ocamlVersion = "4_12"; };
   build_4_13 = buildCandidates { inherit pkgs; ocamlVersion = "4_13"; };
-  build_4_14 = buildCandidates { inherit pkgs; ocamlVersion = "4_14"; };
+  # build_4_14 = buildCandidates { inherit pkgs; ocamlVersion = "4_14"; };
   build_5_00 = buildCandidates { inherit pkgs; ocamlVersion = "5_00"; extraIgnores = ocaml5Ignores; };
 
+/*
   arm64_4_13 = (if system == "x86_64-linux" then
     crossTarget pkgs.pkgsCross.aarch64-multiplatform-musl "4_13"
   else
@@ -18,5 +19,7 @@ with filter;
   musl_4_13 = (if system == "x86_64-linux" then
     crossTarget pkgs.pkgsCross.musl64 "4_13"
   else
-    { });
+    { }
+    );
+*/
 }
