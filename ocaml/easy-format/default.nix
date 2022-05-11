53db310c85b0ee23cc8f5c5318d2d9d5db2ee9d3
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-05-11 14:09:31 -0700

    Update easy-format

--- ocaml/easy-format/default.nix
+++ ocaml/easy-format/default.nix
@@ -1,10 +1,10 @@
 { buildDunePackage }:
 
-buildDunePackage rec {
+buildDunePackage {
   pname = "easy-format";
-  version = "1.3.2+500";
+  version = "1.3.3";
   src = builtins.fetchurl {
-    url = https://github.com/ocaml-community/easy-format/archive/4568bfa1887146ab6ce41a48c9e00c8c1329c6e4.tar.gz;
-    sha256 = "1f8pkbcz6y2mjr89k3kr5zimn4s1vvs14c0m3lzd4dwxs5vlz9g9";
+    url = https://github.com/ocaml-community/easy-format/releases/download/1.3.3/easy-format-1.3.3.tbz;
+    sha256 = "05n4mm1yz33h9gw811ivjw7x4m26lpmf7kns9lza4v6227lwmz7a";
   };
 }
