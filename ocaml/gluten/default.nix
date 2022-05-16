2d9ae338bd166d4f84aaea3758ee1b10661eed65
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-05-16 12:20:34 -0700

    Revert "Update gluten (#293)"
    This reverts commit de7704ce998a5b5f99766551527a73248d4e6c0e.

--- ocaml/gluten/default.nix
+++ ocaml/gluten/default.nix
@@ -5,8 +5,8 @@ buildDunePackage {
   version = "0.2.2-dev";
 
   src = builtins.fetchurl {
-    url = https://github.com/anmonteiro/gluten/archive/faa13c5.tar.gz;
-    sha256 = "0d9g92mk508pzh119fgnr1yamvsc0igicbsvdc3glmrlmn4dp7y6";
+    url = https://github.com/anmonteiro/gluten/archive/8d31bd7.tar.gz;
+    sha256 = "1fxhb3hpr29avf4fpkgjmgm0i00ypk2lcw47gbbvk7nx3jlhinbg";
   };
 
   propagatedBuildInputs = [ bigstringaf faraday ke ];
