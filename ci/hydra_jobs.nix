a42be0599f9d36b500e296c2b94eb1db57f9a9da
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-20 08:16:47 +0200

    Declarative hydra config with PRs (#299)
    * Add declarative hydra jobs
    
    * remove unused stuff
    
    * tweaks
    
    * Is it the wrong name?
    
    * Add some debugging logs
    
    * Move to spec.json
    
    * try putting the output directly
    
    * Try different flake url
    
    * asd
    
    * change name
    
    * Add type
    
    * Set type in the generated job
    
    * fmt
    
    * Add a real PR in simple-pr-dummy.json
    
    * Update ci/hydra_spec.json

---
+++ ci/hydra_jobs.nix
@@ -0,0 +1,54 @@
+# Heavily inspired by https://github.com/cleverca22/hydra-configs
+{ pulls ? ./simple-pr-dummy.json }:
+
+let
+  pkgs = import <nixpkgs> { };
+  makeSpec = contents:
+    builtins.derivation {
+      name = "spec.json";
+      system = "x86_64-linux";
+      preferLocalBuild = true;
+      allowSubstitutes = false;
+      builder = "/bin/sh";
+      args = [
+        (builtins.toFile "builder.sh" ''
+          echo "$contents"
+          echo "$out"
+          echo "$contents" > $out
+        '')
+      ];
+      contents = builtins.toJSON contents;
+    };
+in
+with pkgs.lib;
+let
+  defaults = {
+    enabled = 1;
+    hidden = false;
+    keepnr = 1;
+    schedulingshares = 20;
+    checkinterval = 600;
+    enableemail = false;
+    emailoverride = "";
+    type = 1;
+  };
+  primary_jobsets = {
+    nix-overlays = defaults // {
+      keepnr = 2;
+      schedulingshares = 100;
+      description = "nix-overlays";
+      flake = "github:anmonteiro/nix-overlays";
+    };
+  };
+  pr_data = builtins.fromJSON (builtins.readFile pulls);
+  makePr = num: info: {
+    name = "nix-overlays-${num}";
+    value = defaults // {
+      description = "PR ${num}: ${info.title}";
+      flake = "github:anmonteiro/nix-overlays/${info.head.ref}";
+    };
+  };
+  pull_requests = listToAttrs (mapAttrsToList makePr pr_data);
+  jobsetsAttrs = pull_requests // primary_jobsets;
+in
+{ jobsets = makeSpec jobsetsAttrs; }
