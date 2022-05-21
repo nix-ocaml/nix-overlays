54b336938c89c56960cb6ac4c1040333cde4268a
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-21 21:56:02 +0200

    Ignore landmarks for aarch64-darwin

--- ci/hydra.nix
+++ ci/hydra.nix
@@ -1,14 +1,15 @@
 { pkgs, system }:
 let
   filter = pkgs.callPackage ./filter.nix { };
+  extraIgnores = if system == "aarch64-darwin" then filter.aarch64DarwinIgnores else [ ];
 in
 
 with filter;
 {
   # build_4_12 = buildCandidates { inherit pkgs; ocamlVersion = "4_12"; };
-  build_4_13 = buildCandidates { inherit pkgs; ocamlVersion = "4_13"; };
-  build_4_14 = buildCandidates { inherit pkgs; ocamlVersion = "4_14"; };
-  build_5_00 = buildCandidates { inherit pkgs; ocamlVersion = "5_00"; extraIgnores = ocaml5Ignores; };
+  build_4_13 = buildCandidates { inherit pkgs extraIgnores; ocamlVersion = "4_13"; };
+  build_4_14 = buildCandidates { inherit pkgs extraIgnores; ocamlVersion = "4_14"; };
+  build_5_00 = buildCandidates { inherit pkgs; ocamlVersion = "5_00"; extraIgnores = extraIgnores ++ ocaml5Ignores; };
 
   /*
     arm64_4_13 = (if system == "x86_64-linux" then
