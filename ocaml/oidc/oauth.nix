afa9af9f6525ddeb8868816e4bd71b631cdb1193
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-24 16:10:15 +0200

    Upgrade OIDC (#313)
    * Fix hydra
    
    * Update oidc to latest commit

--- ocaml/oidc/oauth.nix
+++
@@ -1,9 +0,0 @@
-{ lib, buildDunePackage, oidc, uri, yojson, logs, base64 }:
-
-buildDunePackage {
-  pname = "oauth";
-  inherit (oidc) src version;
-
-  propagatedBuildInputs = [ uri yojson logs base64 ];
-
-}
