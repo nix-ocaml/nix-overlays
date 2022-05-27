70fb1c00bd49362adc9048f17992f9870c6a12f8
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-05-27 14:59:49 -0700

    Add melange that builds on 4.14

--- ocaml/melange/default.nix
+++ ocaml/melange/default.nix
@@ -15,47 +15,50 @@ let
     lib.versionOlder "4.12" ocaml.version &&
     !(lib.versionOlder "4.13" ocaml.version);
 
+  melange412Deps = [
+    dune-action-plugin
+  ];
 in
 
-if is_412
-then
-  buildDunePackage
-  rec {
-    pname = "melange";
-    version = "0.0.0";
+buildDunePackage {
+  pname = "melange";
+  version = "0.0.0";
 
-    src = builtins.fetchurl {
-      url = https://github.com/melange-re/melange/archive/434941c.tar.gz;
-      sha256 = "16grqf0r1l8yxdxyp6halbfl1h039911743wbbwgly8wa2di06m4";
-    };
+  src =
+    if is_412 then
+      builtins.fetchurl
+        {
+          url = https://github.com/melange-re/melange/archive/434941c.tar.gz;
+          sha256 = "16grqf0r1l8yxdxyp6halbfl1h039911743wbbwgly8wa2di06m4";
+        } else
+      builtins.fetchurl {
+        url = https://github.com/melange-re/melange/archive/f59cc50.tar.gz;
+        sha256 = "1kinsiap725h5gwj663zp52jj95m297bqhhr0r937shj08jyim6c";
+      };
 
-    nativeBuildInputs = [ cppo ];
+  nativeBuildInputs = [ cppo ];
 
-    propagatedBuildInputs = [
-      cmdliner
-      dune-action-plugin
-      melange-compiler-libs
-      reason
-    ];
+  propagatedBuildInputs =
+    [ cmdliner melange-compiler-libs reason ]
+    ++ lib.optionals is_412 melange412Deps;
 
-    installPhase = ''
-      runHook preInstall
-      ${opaline}/bin/opaline -prefix $out -libdir $OCAMLFIND_DESTDIR
+  installPhase = ''
+    runHook preInstall
+    ${opaline}/bin/opaline -prefix $out -libdir $OCAMLFIND_DESTDIR
 
-      cp package.json bsconfig.json $out
-      cp -r ./_build/default/lib/es6 ./_build/default/lib/js $out/lib
+    cp package.json bsconfig.json $out
+    cp -r ./_build/default/lib/es6 ./_build/default/lib/js $out/lib
 
-      mkdir -p $out/lib/melange
-      cd $out/lib/melange
+    mkdir -p $out/lib/melange
+    cd $out/lib/melange
 
-      tar xvf $OCAMLFIND_DESTDIR/melange/libocaml.tar.gz
-      mv others/* .
-      mv runtime/* .
-      mv stdlib-412/stdlib_modules/* .
-      mv stdlib-412/* .
-      rm -rf others runtime stdlib-412
+    tar xvf $OCAMLFIND_DESTDIR/melange/libocaml.tar.gz
+    mv others/* .
+    mv runtime/* .
+    mv stdlib-412/stdlib_modules/* .
+    mv stdlib-412/* .
+    rm -rf others runtime stdlib-412
 
-      runHook postInstall
-    '';
-  }
-else null
+    runHook postInstall
+  '';
+}
