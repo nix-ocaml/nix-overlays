0aff9e5a7d7fc81531b169fd7729866650f81af0
Ulrik Strid <ulrik.strid@outlook.com>
2022-06-22 08:58:21 +0200

    Fix darwinIgnores on hydra

--- ci/hydra.nix
+++ ci/hydra.nix
@@ -1,7 +1,7 @@
 { pkgs, system }:
 let
   filter = pkgs.callPackage ./filter.nix { };
-  extraIgnores = [ ];
+  extraIgnores = if builtins.hasSuffix "darwin" system then filter.darwinIgnores else [ ];
 in
 
 with filter;
@@ -12,15 +12,15 @@ with filter;
   build_5_00 = ocamlCandidates { inherit pkgs; ocamlVersion = "5_00"; extraIgnores = extraIgnores ++ ocaml5Ignores; };
 
   /*
-    arm64_4_13 = (if system == "x86_64-linux" then
+  arm64_4_13 = (if system == "x86_64-linux" then
     crossTarget pkgs.pkgsCross.aarch64-multiplatform-musl "4_13"
-    else
+  else
     { });
 
-    musl_4_13 = (if system == "x86_64-linux" then
+  musl_4_13 = (if system == "x86_64-linux" then
     crossTarget pkgs.pkgsCross.musl64 "4_13"
-    else
+  else
     { }
-    );
+  );
   */
 }
