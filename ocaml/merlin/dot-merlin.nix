a7e5c577f391ee039f71e7a63f62d3f6720fd926
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-04 13:06:48 -0700

    update merlin / lwt

--- ocaml/merlin/dot-merlin.nix
+++ ocaml/merlin/dot-merlin.nix
@@ -4,49 +4,31 @@
 , yojson
 , csexp
 , result
+, merlin-lib
 }:
-
-let
-  merlinVersion = "4.4";
-
-  ocamlVersionShorthand = lib.concatStrings
-    (lib.take 2 (lib.splitVersion ocaml.version));
-
-  version = "${merlinVersion}-${ocamlVersionShorthand}";
-
-in
 
 buildDunePackage {
   pname = "dot-merlin-reader";
-  version = version;
+  version =
+    if lib.versionAtLeast ocaml.version "4.14" then
+      merlin-lib.version else "n/a";
+
   src =
-    if (lib.versionOlder "5.00" ocaml.version)
-    then
-      builtins.fetchurl
-        {
-          url = https://github.com/ocaml/merlin/archive/fce8fb220c6677a4e1f1677efa2e5b5b13f12254.tar.gz;
-          sha256 = "1n53b7isla3jyva3kpizxrmyjja6w62c2p799lzla2jy96ldjpcj";
-        }
-    else if (lib.versionOlder "4.14" ocaml.version)
-    then
-      builtins.fetchurl
-        {
-          url = https://github.com/ocaml/merlin/archive/82723b88659112e76f8da78830243e4c557c60f5.tar.gz;
-          sha256 = "1yd70s78a3csacmn9zs9z2qhqghc6844ly5c5lzgghib304sw75l";
-        }
+    if lib.versionAtLeast ocaml.version "4.14" then
+      merlin-lib.src
     else if (lib.versionOlder "4.13" ocaml.version)
     then
       builtins.fetchurl
         {
-          url = https://github.com/ocaml/merlin/releases/download/v4.4-413/merlin-4.4-413.tbz;
-          sha256 = "1ilmh2gqpwgr51w2ba8r0s5zkj75h00wkw4az61ssvivn9jxr7k0";
+          url = https://github.com/ocaml/merlin/releases/download/v4.6-413/merlin-4.6-413.tbz;
+          sha256 = "1vlhgljcsdnyh9c5kxjvhwn24wbm56bmwddcv3pmzs64hhgkgpgk";
         }
     else if (lib.versionOlder "4.12" ocaml.version)
     then
       builtins.fetchurl
         {
-          url = https://github.com/ocaml/merlin/releases/download/v4.4-412/merlin-4.4-412.tbz;
-          sha256 = "18xjpsiz7xbgjdnsxfc52l7yfh22harj0birlph4xm42d14pkn0n";
+          url = https://github.com/ocaml/merlin/releases/download/v4.6-412/merlin-4.6-412.tbz;
+          sha256 = "15vbckvxr9wgbwfx96h4ly9xab1w95xa0y04874jk6lnnynaxj4a";
         }
     else if (lib.versionOlder "4.11" ocaml.version)
     then
@@ -61,12 +43,5 @@ buildDunePackage {
         sha256 = "109ai1ggnkrwbzsl1wdalikvs1zx940m6n65jllxj68in6bvidz1";
       };
 
-  postPatch = ''
-    substituteInPlace src/utils/std.ml --replace "= capitalize" "= capitalize_ascii"
-    substituteInPlace src/utils/std.ml --replace "= uncapitalize" "= uncapitalize_ascii"
-    substituteInPlace src/utils/std.ml --replace "= lowercase" "= lowercase_ascii"
-    substituteInPlace src/utils/std.ml --replace "= uppercase" "= uppercase_ascii"
-  '';
-
-  buildInputs = [ yojson csexp result ];
+  propagatedBuildInputs = [ yojson csexp result merlin-lib ];
 }
---
+++ ocaml/merlin/lib.nix
@@ -0,0 +1,36 @@
+{ lib
+, ocaml
+, buildDunePackage
+, yojson
+, csexp
+, result
+}:
+
+let
+  merlinVersion = "4.6";
+
+  ocamlVersionShorthand = lib.concatStrings
+    (lib.take 2 (lib.splitVersion ocaml.version));
+
+  version = "${merlinVersion}-${ocamlVersionShorthand}";
+in
+
+buildDunePackage {
+  pname = "merlin-lib";
+  version = version;
+  src =
+    if (lib.versionOlder "5.00" ocaml.version)
+    then
+      builtins.fetchurl
+        {
+          url = https://github.com/ocaml/merlin/archive/91070ee.tar.gz;
+          sha256 = "05m3dq9w99lvadyzcfwgmclzr9x4zmghyamkr13j7nsdi014n2r4";
+        }
+    else
+      builtins.fetchurl {
+        url = https://github.com/ocaml/merlin/releases/download/v4.6-414/merlin-4.6-414.tbz;
+        sha256 = "1klk0d0j8lzzsp9hlkm8bqafsj54fh3w9r6vcc0r9n3p7h5dgsq2";
+      };
+
+  buildInputs = [ yojson csexp result ];
+}
