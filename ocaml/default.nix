6ac30dd1175d390f11c18267cda70e16ffa4a1d2
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-06 20:07:27 -0700

    fmt 0.9 wasn't broken.

--- ocaml/default.nix
+++ ocaml/default.nix
@@ -500,6 +500,10 @@ with oself;
   flow_parser = callPackage ./flow_parser { };
 
   fmt = osuper.fmt.overrideAttrs (o: {
+    src = fetchurl {
+      url = https://erratique.ch/software/fmt/releases/fmt-0.9.0.tbz;
+      sha256 = "0q8j2in2473xh7k4hfgnppv9qy77f2ih89yp6yhpbp92ba021yzi";
+    };
     # reverts https://github.com/NixOS/nixpkgs/pull/178306
     propagatedBuildInputs = o.propagatedBuildInputs ++ [ cmdliner ];
   });
