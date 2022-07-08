7c3cc1ea495c808deaa7dc3778ba8b4490a1d084
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-08 15:21:19 -0700

    try parallelizing CI runs

--- ci/default.nix
+++ ci/default.nix
@@ -1,5 +1,6 @@
-{ ocamlVersion, target ? "native" }:
+{ ocamlVersion, target ? "native", index ? null }:
 let
+  parallelism = 4;
 
   system = builtins.currentSystem;
   flake = (import
@@ -17,25 +18,40 @@ let
   filter = pkgs.callPackage ./filter.nix { };
   inherit (pkgs) lib stdenv pkgsCross;
 
+  package-set = {
+    top-level-packages = (with pkgs; [
+      cockroachdb-22_x
+      # mongodb-4_2
+      esy
+    ] ++ lib.optional stdenv.isLinux pkgs.kubernetes);
+
+    native = (lib.attrValues (filter.ocamlCandidates {
+      inherit pkgs ocamlVersion;
+      extraIgnores =
+        lib.optionals (ocamlVersion == "5_00") filter.ocaml5Ignores
+          ++ lib.optionals (ocamlVersion == "4_12") filter.ocaml412Ignores;
+    }))
+    ++ lib.optional (ocamlVersion == "4_14") pkgs.ocamlPackages.melange;
+
+    musl = filter.crossTargetList pkgsCross.musl64 ocamlVersion;
+
+    arm64 = filter.crossTargetList pkgsCross.aarch64-multiplatform-musl ocamlVersion;
+  };
+
+  allPackages = package-set.${target};
 in
 
-{
-  top-level-packages = (with pkgs; [
-    cockroachdb-22_x
-    # mongodb-4_2
-    esy
-  ] ++ lib.optional stdenv.isLinux pkgs.kubernetes);
-
-  native = (lib.attrValues (filter.ocamlCandidates {
-    inherit pkgs ocamlVersion;
-    extraIgnores =
-      lib.optionals (ocamlVersion == "5_00") filter.ocaml5Ignores
-        ++ lib.optionals (ocamlVersion == "4_12") filter.ocaml412Ignores;
-  }))
-  ++ lib.optional (ocamlVersion == "4_14") pkgs.ocamlPackages.melange;
-
-  musl = filter.crossTargetList pkgsCross.musl64 ocamlVersion;
-
-  arm64 = filter.crossTargetList pkgsCross.aarch64-multiplatform-musl ocamlVersion;
-
-}."${target}"
+if index == null then allPackages
+else
+  let
+    len = lib.length allPackages;
+    partitionLen = len / parallelism;
+    dropped = (lib.drop (index * partitionLen) allPackages);
+  in
+  lib.trace
+    ''
+      len: ${builtins.toString len};
+      dropped: ${builtins.toString (lib.length dropped)};
+      partitionLen: ${builtins.toString partitionLen};
+    ''
+    (lib.take partitionLen dropped)
