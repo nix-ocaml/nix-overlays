f8d5e51b4a4c0257eab27d63a0a4763906decc4c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-27 10:16:14 -0700

    remove old ocamlformat-rpc

--- ocaml/default.nix
+++ ocaml/default.nix
@@ -940,8 +940,6 @@ with oself;
   inherit (callPackage ./ocamlformat-rpc { })
     # latest version
     ocamlformat-rpc
-    ocamlformat-rpc_0_20_0
-    ocamlformat-rpc_0_20_1
     ocamlformat-rpc_0_21_0;
 
   ocaml_sqlite3 = osuper.ocaml_sqlite3.overrideAttrs (o: {
--- ocaml/ocamlformat-rpc/default.nix
+++ ocaml/ocamlformat-rpc/default.nix
@@ -4,16 +4,6 @@
 let mkocamlformat-rpc = callPackage ./generic.nix; in
 
 rec {
-  ocamlformat-rpc_0_20_0 = mkocamlformat-rpc {
-    version = "0.20.0";
-    ocamlformat = null;
-  };
-
-  ocamlformat-rpc_0_20_1 = mkocamlformat-rpc {
-    version = "0.20.1";
-    ocamlformat = null;
-  };
-
   ocamlformat-rpc_0_21_0 =
     if lib.versionAtLeast ocaml.version "4.13" then
       mkocamlformat-rpc { version = "0.21.0"; }
@@ -21,6 +11,6 @@ rec {
 
   ocamlformat-rpc =
     if lib.versionAtLeast ocaml.version "4.13" then
-      ocamlformat-rpc_0_21_0
-    else ocamlformat-rpc_0_20_1;
+      ocamlformat-rpc_0_21_0 else null;
+
 }
