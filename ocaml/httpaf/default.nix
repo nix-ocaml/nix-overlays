4a29fd8cd76a63bc8c0c36f817dae9870b320a8a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-08-09 12:48:11 -0700

    update httpaf /h2

--- ocaml/httpaf/default.nix
+++ ocaml/httpaf/default.nix
@@ -5,7 +5,7 @@ buildDunePackage {
   pname = "httpaf";
   propagatedBuildInputs = [ angstrom faraday ];
   src = builtins.fetchurl {
-    url = https://github.com/anmonteiro/httpaf/archive/395aad2bc4.tar.gz;
-    sha256 = "0m7nfynmwikxixaa1y0k44ngy5s2j2zf906q6v7vcvgd1igh9a1m";
+    url = https://github.com/anmonteiro/httpaf/archive/893ca40760.tar.gz;
+    sha256 = "0mw6clix5jaq9m4ipi5026l7s2ha4nn0kczag5c9pn9v7wsva6lx";
   };
 }
