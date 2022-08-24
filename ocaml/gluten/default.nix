f81847e16f593f8d2d4fc803d8e4f7fe92899a22
EduardoRFS <theeduardorfs@gmail.com>
2022-08-24 17:53:52 -0300

    pin gluten to latest commit

--- ocaml/gluten/default.nix
+++ ocaml/gluten/default.nix
@@ -1,13 +1,14 @@
-{ buildDunePackage, bigstringaf, faraday, ke }:
+{ fetchFromGitHub, buildDunePackage, bigstringaf, faraday, ke }:
 
 buildDunePackage {
   pname = "gluten";
   version = "0.3.0";
 
-  src = builtins.fetchurl {
-    url = https://github.com/anmonteiro/gluten/archive/0.3.0.tar.gz;
-    sha256 = "1q77iw46hhlp38c8c0lb1a0sh3bn1g2x9cdpf3zd2cq3lr20i482";
+  src = fetchFromGitHub {
+    owner = "anmonteiro";
+    repo = "gluten";
+    rev = "94e25a211f07ee6c5375669821dfc728bfc6f158";
+    sha256 = "ZPRff2ZLnu4QtlfZw0Nwyo172ocsK8yzBDl2EDfhuw0=";
   };
-
   propagatedBuildInputs = [ bigstringaf faraday ke ];
 }
