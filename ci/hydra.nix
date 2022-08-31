dc8632c2d5a992ec189cc264b1a7b95cc8879588
Ulrik Strid <ulrik.strid@outlook.com>
2022-08-31 22:09:01 +0200

    Only build 4.14 and 5.00 in Hydra (#422)

--- ci/hydra.nix
+++ ci/hydra.nix
@@ -7,17 +7,16 @@ in
 
 with filter;
 {
-  build_4_13 = ocamlCandidates { inherit pkgs extraIgnores; ocamlVersion = "4_13"; };
   build_4_14 = ocamlCandidates { inherit pkgs extraIgnores; ocamlVersion = "4_14"; };
   build_5_00 = ocamlCandidates { inherit pkgs; ocamlVersion = "5_00"; extraIgnores = extraIgnores ++ ocaml5Ignores; };
 
-  arm64_4_13 = (if system == "x86_64-linux" then
-    crossTarget pkgs.pkgsCross.aarch64-multiplatform-musl "4_13"
+  arm64_4_14 = (if system == "x86_64-linux" then
+    crossTarget pkgs.pkgsCross.aarch64-multiplatform-musl "4_14"
   else
     { });
 
-  musl_4_13 = (if system == "x86_64-linux" then
-    crossTarget pkgs.pkgsCross.musl64 "4_13"
+  musl_4_14 = (if system == "x86_64-linux" then
+    crossTarget pkgs.pkgsCross.musl64 "4_14"
   else
     { }
   );
