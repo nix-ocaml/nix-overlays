b2ce484edab24b226dc72cbe5af0dec7aeb26b2a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-09-27 23:17:58 -0700

    update gluten and eio-ssl

--- ocaml/eio-ssl/default.nix
+++ ocaml/eio-ssl/default.nix
@@ -4,8 +4,8 @@ buildDunePackage {
   pname = "eio-ssl";
   version = "n/a";
   src = builtins.fetchurl {
-    url = https://github.com/anmonteiro/eio-ssl/archive/7aefffb.tar.gz;
-    sha256 = "0s457k48midisizara20hq5awfq4jql1rp1rp8hpj4snwy6l592g";
+    url = https://github.com/anmonteiro/eio-ssl/archive/1d3aaa0b.tar.gz;
+    sha256 = "0qx12147vrr5al15s2xmmh27hl763igv8lvwsrf0kjf21mcwvb4q";
   };
   propagatedBuildInputs = [ ssl eio_main ];
 }
--- ocaml/gluten/default.nix
+++ ocaml/gluten/default.nix
@@ -5,8 +5,8 @@ buildDunePackage {
   version = "0.3.0";
 
   src = builtins.fetchurl {
-    url = https://github.com/anmonteiro/gluten/archive/1e5c83a1.tar.gz;
-    sha256 = "1iriyvkc2l6yp2k2np8c8y1zyiv46png4mg9cfmd2401g0gc8aph";
+    url = https://github.com/anmonteiro/gluten/archive/29de1f333.tar.gz;
+    sha256 = "1glbb7wbs2s4q7dddr8s4ziz12qh9bkc8dg4v4lkc8p9dvllhvw1";
   };
   propagatedBuildInputs = [ bigstringaf faraday ke ];
 }
