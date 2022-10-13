0daa0729115ad44f1360184af81cdc841a9b0e9b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-10-13 11:58:58 -0700

    upgrade ppxlib to 0.28

--- ocaml/calendar/default.nix
+++ ocaml/calendar/default.nix
@@ -4,8 +4,8 @@ buildDunePackage {
   pname = "calendar";
   version = "3.0-dev";
   src = builtins.fetchurl {
-    url = https://github.com/ocaml-community/calendar/archive/26a8c3d7667d49698fb5c7d1d464924aa7476a1d.tar.gz;
-    sha256 = "1r5p5jqv275mcq8qa402wl9k300iy5mv0lyiad9g5cwcs7yzvxac";
+    url = https://github.com/ocaml-community/calendar/archive/refs/tags/v3.0.0.tar.gz;
+    sha256 = "17kfa0gfrv0d5f6zksg7yah8ly5pwyzws483mwvqiwfkc8bx617a";
   };
 
   checkInputs = [ alcotest ];
