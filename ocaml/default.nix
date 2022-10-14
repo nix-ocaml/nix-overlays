280cfc78fa5214e65fac4348d0d2c310a9b9e0b0
Ulrik Strid <ulrik.strid@outlook.com>
2022-10-14 09:21:15 +0200

    pure-splitmix is upstreamed

--- ocaml/default.nix
+++ ocaml/default.nix
@@ -1238,18 +1238,6 @@ with oself;
 
   ptime = (osuper.ptime.override { jsooSupport = false; });
 
-  pure-splitmix = buildDunePackage rec {
-    pname = "pure-splitmix";
-    version = "0.3";
-
-    src = fetchFromGitHub {
-      owner = "Lysxia";
-      repo = pname;
-      rev = version;
-      sha256 = "RUnsAB4hMV87ItCyGhc47bHGY1iOwVv9kco2HxnzqbU=";
-    };
-  };
-
   reanalyze =
     if lib.versionOlder "4.13" osuper.ocaml.version then null
     else
