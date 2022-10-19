874db1c9e9471930053c2d33afd29c017b6ac875
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-10-18 18:15:19 -0700

    update melange

--- ocaml/melange/default.nix
+++ ocaml/melange/default.nix
@@ -11,35 +11,35 @@
 , ocaml
 }:
 
-buildDunePackage {
+buildDunePackage rec {
   pname = "melange";
   version = "0.0.0";
 
   src = builtins.fetchurl {
-    url = https://github.com/melange-re/melange/archive/98ac719.tar.gz;
-    sha256 = "0fnyrjii02ak6q0gbp76q6albfbd86pbmjz0ddgzf93fil40fdg6";
+    url = https://github.com/melange-re/melange/archive/066c5ebc2.tar.gz;
+    sha256 = "0kglmvkb2hmg5s30nzh611p20qgsj03v3clc5f36r4kr2w33m05c";
   };
 
   nativeBuildInputs = [ cppo ];
 
-  propagatedBuildInputs = [ cmdliner melange-compiler-libs reason luv base64 ];
+  propagatedBuildInputs = [
+    cmdliner
+    melange-compiler-libs
+    reason
+    luv
+    base64
+  ];
 
   installPhase = ''
     runHook preInstall
-    ${opaline}/bin/opaline -prefix $out -libdir $OCAMLFIND_DESTDIR
+    dune install --prefix $out --libdir $out/lib ${pname}
 
     cp package.json bsconfig.json $out
-    cp -r ./_build/default/lib/es6 ./_build/default/lib/js $out/lib
 
-    mkdir -p $out/lib/melange
-    cd $out/lib/melange
-
-    tar xvf $OCAMLFIND_DESTDIR/melange/libocaml.tar.gz
-    mv others/* .
-    mv runtime/* .
-    mv stdlib-412/stdlib_modules/* .
-    mv stdlib-412/* .
-    rm -rf others runtime stdlib-412 stdlib_modules
+    mv $out/lib/melange/js $out/lib/js
+    mv $out/lib/melange/es6 $out/lib/es6
+    mv $out/lib/melange/melange/* $out/lib/melange
+    rm -rf $out/lib/melange/melange
 
     runHook postInstall
   '';
