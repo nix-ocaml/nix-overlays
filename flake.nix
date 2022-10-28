046063ead93476aaf064663d44c015aeb186c4db
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-10-27 18:07:26 -0700

    remove updated packages

--- flake.nix
+++ flake.nix
@@ -49,8 +49,10 @@
           } // attrs);
         in
           /*
-            You might read https://nixos.org/manual/nixpkgs/stable/#sec-overlays-argument and want to change this
-            but because of how we're doing overlays we will be overriding any extraOverlays if we don't use `appendOverlays`
+            You might read
+            https://nixos.org/manual/nixpkgs/stable/#sec-overlays-argument and
+            want to change this but because of how we're doing overlays we will
+            be overriding any extraOverlays if we don't use `appendOverlays`
           */
         pkgs.appendOverlays extraOverlays;
 
