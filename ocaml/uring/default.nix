fa35ce35913764bfc15f4b088f80e6d35604e36e
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-11-03 15:38:55 -0700

    Update uring / eio

--- ocaml/uring/default.nix
+++ ocaml/uring/default.nix
@@ -5,8 +5,8 @@ buildDunePackage {
   version = "0.4";
 
   src = builtins.fetchurl {
-    url = https://github.com/ocaml-multicore/ocaml-uring/releases/download/v0.4/uring-0.4.tbz;
-    sha256 = "0pwf26ddys1qy3gqwsqsji8bmg959azab2lysyz2d5hmnh21jcks";
+    url = https://github.com/ocaml-multicore/ocaml-uring/archive/385bf2e1.tar.gz;
+    sha256 = "0bw4c5j9sy0biah447cdj2gbmn84dch9k7a95vwh8kyidnvwrwzh";
   };
 
   postPatch = ''
