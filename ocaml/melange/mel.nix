67b4547e9fc41afabff8834fed37ef18f32adc5b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-11-07 09:14:11 -0800

    update topkg (#541)
    * update topkg
    
    * update melange

--- ocaml/melange/mel.nix
+++ ocaml/melange/mel.nix
@@ -1,9 +1,20 @@
-{ buildDunePackage, cmdliner, melange, luv, cppo }:
+{ buildDunePackage
+, cmdliner
+, melange
+, luv
+, cppo
+, ocaml-migrate-parsetree-2
+}:
 
 buildDunePackage {
   pname = "mel";
   inherit (melange) src version;
 
   nativeBuildInputs = [ cppo ];
-  propagatedBuildInputs = [ cmdliner melange luv ];
+  propagatedBuildInputs = [
+    cmdliner
+    luv
+    ocaml-migrate-parsetree-2
+    melange
+  ];
 }
