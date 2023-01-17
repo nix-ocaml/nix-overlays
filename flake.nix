8384ccfa4f2e185fcc9b757925f823367c2aa7e2
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-01-17 14:55:05 -0800

    self

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
     } // flake-utils.lib.eachDefaultSystem (system:
       {
         legacyPackages = self.makePkgs { inherit system; };
-        packages = { default = hello; };
+        packages = { default = self.legacyPackages.hello; };
 
         overlays = (final: prev:
           let
