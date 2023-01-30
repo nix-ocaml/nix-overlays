cc1fab8da532ef78787071b6bf4534e7025601a8
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-01-30 14:46:38 -0800

    h2

--- ocaml/h2/hpack.nix
+++ ocaml/h2/hpack.nix
@@ -1,11 +1,14 @@
-{ buildDunePackage, angstrom, faraday }:
+{ fetchFromGitHub, buildDunePackage, angstrom, faraday }:
 
 buildDunePackage {
   pname = "hpack";
   version = "0.10.0-dev";
-  src = builtins.fetchurl {
-    url = https://github.com/anmonteiro/ocaml-h2/archive/c736ca59.tar.gz;
-    sha256 = "15frl9iw7vnp6cd9r6das39wij39c2536ydcw6b5bhwhjsbrgj67";
+
+  src = fetchFromGitHub {
+    owner = "anmonteiro";
+    repo = "ocaml-h2";
+    rev = "c736ca59";
+    sha256 = "sha256-5jwtoeDiQkUfZaxSRgFUaw4s+MeUGyivi3x7ELh3+1k=";
   };
   propagatedBuildInputs = [ angstrom faraday ];
 }
