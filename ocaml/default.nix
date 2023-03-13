82e026346faad73306007b9cd999107f4221ed9c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-03-13 12:11:48 -0700

    actually disable for real now_production_2

--- ocaml/default.nix
+++ ocaml/default.nix
@@ -964,7 +964,7 @@ with oself;
     buildInputs = [ libev-oc ];
   };
   lev-fiber =
-    if lib.versionAtLeast "4.14" osuper.ocaml.version then
+    if lib.versionAtLeast osuper.ocaml.version "4.14" then
       buildDunePackage
         {
           pname = "lev-fiber";
@@ -973,7 +973,7 @@ with oself;
           checkInputs = [ ppx_expect ];
         } else null;
   lev-fiber-csexp =
-    if lib.versionAtLeast "4.14" osuper.ocaml.version then
+    if lib.versionAtLeast osuper.ocaml.version "4.14" then
       buildDunePackage
         {
           pname = "lev-fiber";
