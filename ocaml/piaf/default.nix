234ef8bfa7fee589c9479db7af529bbe01ed74c9
Ulrik Strid <ulrik.strid@outlook.com>
2023-03-15 11:41:59 +0100

    Use newer apple_sdk on x86_64-darwin

--- ocaml/piaf/default.nix
+++ ocaml/piaf/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , lib
+, darwin
 , fetchFromGitHub
 , buildDunePackage
 , eio
@@ -40,7 +41,9 @@ buildDunePackage {
     multipart_form
     uri
     websocketaf
-  ];
+  ] ++ lib.optionals (stdenv.isDarwin && !stdenv.isAarch64) (with darwin.apple_sdk_11_0; [
+    Libsystem
+  ]);
 
   meta = {
     description = "An HTTP library with HTTP/2 support written entirely in OCaml";
