c78572ae9e52c48c0f6af7feafc6237cc86be206
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-04-15 02:03:03 -0700

    fix merlin

--- ocaml/merlin/lib.nix
+++ ocaml/merlin/lib.nix
@@ -32,5 +32,11 @@ buildDunePackage {
         sha256 = "1bwbg6k9i1vjf5br5dbn7fd0xwbi86rxwy1dycwnspafqn2xdi8w";
       };
 
+  postPatch =
+    if lib.hasPrefix "5_" ocaml.version then ''
+      substituteInPlace src/config/gen_config.ml --replace \
+        '| `OCaml_5_0_0 ]' '| `OCaml_5_0_0 | `OCaml_5_1_0 ]'
+    '' else "";
+
   buildInputs = [ yojson csexp result ];
 }
