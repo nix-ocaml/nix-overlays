3946323dab5d89264b9add648c2927abca58079f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-04-23 17:45:12 -0700

    update flow parser

--- ocaml/flow_parser/default.nix
+++ ocaml/flow_parser/default.nix
@@ -6,11 +6,10 @@ buildDunePackage {
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "flow";
-    rev = "v0.186.0";
-    sha256 = "sha256-Bip56IgE+XtNSwUC09ANe9ClSg6vTQO60spt1ijvs68=";
+    rev = "v0.204.0";
+    hash = "sha256-263ZbEDGiZI/2dSLxs966+wtSHG2QMnTtzJ7hPQ4Ix8=";
   };
 
-  patches = [ ./flow.patch ];
   propagatedBuildInputs = [
     base
     ppx_deriving
