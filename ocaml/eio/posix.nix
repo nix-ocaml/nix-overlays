2f3001e986cfcfae13082ea0fd9beecb437e6aa7
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-04-30 23:13:10 -0700

    fix

--- ocaml/eio/posix.nix
+++ ocaml/eio/posix.nix
@@ -1,9 +1,20 @@
-{ buildDunePackage, dune-configurator, eio, iomux, logs, fmt }:
+{ buildDunePackage
+, darwin
+, dune-configurator
+, eio
+, lib
+, stdenv
+, iomux
+, logs
+, fmt
+}:
 
 buildDunePackage {
   pname = "eio_posix";
   inherit (eio) version src;
 
   buildInputs = [ dune-configurator ];
-  propagatedBuildInputs = [ eio logs fmt iomux ];
+  propagatedBuildInputs = [ eio logs fmt iomux ] ++
+    lib.optionals (!(lib.versionAtLeast "11.0.0" darwin.apple_sdk.Libsystem.version))
+      [ darwin.apple_sdk_11_0.Libsystem ];
 }
