ba6d7467fe457e53800440777e5cd1d467e4b3e6
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-05-11 22:29:41 -0700

    update ppx_rapper

--- ocaml/ppx_rapper/default.nix
+++ ocaml/ppx_rapper/default.nix
@@ -5,10 +5,10 @@ buildDunePackage rec {
   version = "3.0.0";
 
   src = fetchFromGitHub {
-    owner = "anmonteiro";
+    owner = "roddyyaga";
     repo = "ppx_rapper";
-    rev = "d51ad5e45769e4a347806cdba3cf08da8a791248";
-    hash = "sha256-u7aMjIBYhfKf09BpVrH/Lco+YI5IWjnlmMKGKk1JR28=";
+    rev = "03fd3d47b4fd143766c07e4a4a696a057945f8da";
+    hash = "sha256-RxZeknsuQrW4OEW8b0JD/K/Jex6K6C9/q1w2FlpDw7g=";
   };
 
   propagatedBuildInputs = [ caqti pg_query ];
