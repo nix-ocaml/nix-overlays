d7df81bb62ec028a0ee945e94d9ef2667e22e55b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-05-25 12:00:05 -0700

    Update sources (#841)
    * Update sources
    
    * fix dune-rpc-lwt
    
    ---------
    
    Co-authored-by: UpdateBot <ulrikstrid@users.noreply.github.com>

--- ocaml/dune/rpc-lwt.nix
+++ ocaml/dune/rpc-lwt.nix
@@ -1,17 +1,11 @@
-{ lib, buildDunePackage, dune, dune-rpc, lwt, csexp, dyn }:
+{ lib, buildDunePackage, dune, dune-rpc, lwt, csexp, dyn, result }:
 
 
 buildDunePackage {
   pname = "dune-rpc-lwt";
   inherit (dune) src version;
 
-  propagatedBuildInputs = [ csexp dune-rpc lwt ];
+  propagatedBuildInputs = [ csexp dune-rpc lwt result ];
   dontAddPrefix = true;
   inherit (dyn) preBuild;
-
-  postPatch = ''
-    substituteInPlace "otherlibs/dune-rpc-lwt/src/dune_rpc_lwt.ml" --replace \
-      "Lwt_result.catch (Lwt_io.with_file ~mode:Input s Lwt_io.read)" \
-      "Lwt_result.catch (fun () -> (Lwt_io.with_file ~mode:Input s Lwt_io.read))"
-  '';
 }
