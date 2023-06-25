59df23cb9b77adb6398b769001cf2c8ae99f435c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-06-24 17:07:48 -0700

    fix dune + jst

--- ocaml/overlay-ocaml-packages.nix
+++ ocaml/overlay-ocaml-packages.nix
@@ -61,18 +61,33 @@ let
         };
       };
 
-      ocamlPackages_jst = newOCamlScope {
-        major_version = "4";
-        minor_version = "14";
-        patch_version = "1+jst";
-        hardeningDisable = [ "strictoverflow" ];
-        src = super.fetchFromGitHub {
-          owner = "ocaml-flambda";
-          repo = "ocaml-jst";
-          rev = "e3076d2e7321a8e8ff18e560ed7a55d6ff0ebf04";
-          hash = "sha256-y5p73ZZtwkgUzvCHlE9nqA2OdlDbYWr8wnWRhYH82hE=";
-        };
-      };
+      ocamlPackages_jst = ocaml-ng.ocamlPackages_4_14.overrideScope' (oself: osuper: {
+        ocaml = (callPackage
+          (import "${nixpkgs}/pkgs/development/compilers/ocaml/generic.nix" {
+            major_version = "4";
+            minor_version = "14";
+            patch_version = "1+jst";
+          })
+          { }).overrideAttrs (o: {
+          src = super.fetchFromGitHub {
+            owner = "ocaml-flambda";
+            repo = "ocaml-jst";
+            rev = "e3076d2e7321a8e8ff18e560ed7a55d6ff0ebf04";
+            hash = "sha256-y5p73ZZtwkgUzvCHlE9nqA2OdlDbYWr8wnWRhYH82hE=";
+          };
+          hardeningDisable = [ "strictoverflow" ];
+        });
+
+        dune_3 = osuper.dune_3.overrideAttrs (_: {
+          postPatch = ''
+            substituteInPlace boot/bootstrap.ml --replace 'v >= (5, 0, 0)' "true"
+            substituteInPlace boot/duneboot.ml --replace 'ocaml_version >= (5, 0)' "true"
+            substituteInPlace src/ocaml-config/ocaml_config.ml --replace 'version >= (5, 0, 0)' "true"
+            substituteInPlace src/ocaml/version.ml --replace 'version >= (5, 0, 0)' "true"
+          '';
+        });
+      });
+
     } else { });
 
   overlaySinglePackageSet = pkgSet:
