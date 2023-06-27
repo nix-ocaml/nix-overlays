5ffa9e28a1436063270c75d3884b2794663009ec
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-06-27 00:30:52 -0700

    fix websocketaf

--- ocaml/websocketaf/default.nix
+++ ocaml/websocketaf/default.nix
@@ -6,8 +6,8 @@ buildDunePackage {
   src = fetchFromGitHub {
     owner = "anmonteiro";
     repo = "websocketaf";
-    rev = "890a21453181a2a5230d598a87c2741defb2ade0";
-    hash = "sha256-sNKEI4XnpBHL31OoKb+wZZGYeKEv0tM658igsxZeZww=";
+    rev = "c064e2641f3a878d621c1f705696aa8b6b246944";
+    hash = "sha256-AEOUEwYfl3t6tproEusj+wbh4cIjb912YXl4aKeak9M=";
   };
 
   propagatedBuildInputs = [ angstrom faraday gluten httpaf base64 ];
