548c3a021e6d990773524c205005b5881847fd59
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-07-05 19:41:52 -0700

    fix more

--- ocaml/cookie/session-lwt.nix
+++ ocaml/cookie/session-lwt.nix
@@ -1,10 +1,10 @@
-{ lib, buildDunePackage, cookie, session-cookie, lwt }:
+{ lib, buildDunePackage, cookie, session-cookie, lwt, result }:
 
 buildDunePackage {
   pname = "session-cookie-lwt";
   inherit (cookie) src version;
 
-  propagatedBuildInputs = [ session-cookie lwt ];
+  propagatedBuildInputs = [ session-cookie lwt result ];
 
   meta = {
     description = "Session handling based on Cookie parsing and serialization for OCaml";
