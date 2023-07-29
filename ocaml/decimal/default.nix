d0bed5a0d97eb1695a2069f105a27d91321f2147
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-07-29 10:12:54 -0700

    update decimal (#975)

--- ocaml/decimal/default.nix
+++ ocaml/decimal/default.nix
@@ -1,11 +1,11 @@
 { buildDunePackage, zarith }:
 
-buildDunePackage rec {
+buildDunePackage {
   pname = "decimal";
-  version = "0.2.1";
+  version = "1.0.0";
   src = builtins.fetchurl {
-    url = "https://github.com/yawaramin/ocaml-decimal/releases/download/v${version}/decimal-v${version}.tbz";
-    sha256 = "1iwqkx35xaynzx5xwxman10g9wizc3v689kb8r24l6d1lg5in27y";
+    url = https://github.com/yawaramin/ocaml-decimal/releases/download/v1.0.0/decimal-1.0.0.tbz;
+    sha256 = "09b5mrajni1ha39v7zv08w5jjf2mnbsgg3wj3j0mhl9mmcxvwhf6";
   };
 
   propagatedBuildInputs = [ zarith ];
