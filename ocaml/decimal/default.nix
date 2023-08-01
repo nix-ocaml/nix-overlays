bb3cd24a0bf637a8ab06f7effa6b956d9bed2027
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-08-01 08:15:57 -0700

    rm upstreamed

--- ocaml/decimal/default.nix
+++ ocaml/decimal/default.nix
@@ -5,7 +5,7 @@ buildDunePackage {
   version = "1.0.0";
   src = builtins.fetchurl {
     url = https://github.com/yawaramin/ocaml-decimal/releases/download/v1.0.0/decimal-1.0.0.tbz;
-    sha256 = "09b5mrajni1ha39v7zv08w5jjf2mnbsgg3wj3j0mhl9mmcxvwhf6";
+    sha256 = "06x69a4dragd5kmm4c276f50878bbysrnlqvima8ix5lkxwnchb0";
   };
 
   propagatedBuildInputs = [ zarith ];
