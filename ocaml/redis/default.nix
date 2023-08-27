3f99ee3e728b1fd864e0ac41190f93a5c286c1ac
DolphinChips <DoIphinChips@protonmail.com>
2023-08-27 16:46:05 +0500

    ocaml/redis: make fetched sources match package version (#1020)

--- ocaml/redis/default.nix
+++ ocaml/redis/default.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub, buildDunePackage, uuidm, re, stdlib-shims }:
 
-buildDunePackage {
+buildDunePackage rec {
   pname = "redis";
-  version = "0.4";
+  version = "0.6";
   src = fetchFromGitHub {
     owner = "0xffea";
     repo = "ocaml-redis";
-    rev = "v0.6";
+    rev = "v${version}";
     sha256 = "sha256-+q0fhWW/T/Q7Aof5cMDyVai/DyeMld/C9YEau6LN+J4=";
   };
 
