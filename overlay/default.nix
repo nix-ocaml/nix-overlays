4a1ec12209a6e7a378740868afcc138a653f39b1
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-09-24 14:39:39 -0700

    add hermes js engine

--- overlay/default.nix
+++ overlay/default.nix
@@ -207,6 +207,29 @@ in
       })
       { });
 
+  hermes = stdenv.mkDerivation {
+    name = "hermes";
+    src = super.fetchFromGitHub {
+      owner = "facebook";
+      repo = "hermes";
+      rev = "ee2922a50fb719bdb378025d95dbd32ad93cd679";
+      hash = "sha256-TXTcKAdfnznJQu2YPCRwzDlKMoV/nvp5mpsIrMUmH1c=";
+    };
+    cmakeFlags = lib.optional stdenv.isDarwin [
+      "-DHERMES_BUILD_APPLE_FRAMEWORK=false"
+    ];
+    nativeBuildInputs = with self; [
+      cmake
+      ninja
+      icu
+      python3
+      zip
+      readline
+      git
+    ];
+  };
+
+
   lib = lib // { inherit overlayOCamlPackages; };
 
   inherit (callPackage ../cockroachdb { })
