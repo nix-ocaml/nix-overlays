b826a62ee20d93183a16a8a0fcbfe96a1f38e323
UpdateBot <ulrikstrid@users.noreply.github.com>
2023-11-21 11:35:15 +0000

    Update sources

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=54f00576aa6139a9d54062d0edc2fb31423f0ffb";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=af76d9e3bb03794e4b6f41e304ef038ac65aa277";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
