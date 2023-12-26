a69744261aef68df542f1a82618785e64fa63246
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-12-26 12:27:50 +0000

    try to fix x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -35,6 +35,8 @@
               inherit system;
               overlays = [ overlay ];
               config.allowUnfree = true;
+            } // nixpkgs.lib.optionalAttrs (system == "x86_64-darwin") {
+              config.replaceStdenv = { pkgs, ... }: pkgs.clang11Stdenv;
             } // attrs);
           in
             /*
