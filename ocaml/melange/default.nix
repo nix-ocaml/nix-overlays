88869a1b9d27ce2b18318e44b0d7bd0446fc62bb
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-01-28 20:26:11 -0800

    melange: update to v3.0

--- ocaml/melange/default.nix
+++ ocaml/melange/default.nix
@@ -20,29 +20,20 @@
 
 buildDunePackage {
   pname = "melange";
-  version = "2.1.0";
+  version = "3.0.0";
   duneVersion = "3";
 
   src =
     if (lib.versionOlder "5.1" ocaml.version) then
-      fetchFromGitHub
-        {
-          owner = "melange-re";
-          repo = "melange";
-          rev = "d2715c469e8cf8bf8cff81b897c212094a9517c5";
-          hash = "sha256-Z1l6wyaB9pqQhulSBULh8PJ4+dOaX5SDh1SB8BAc7SM=";
-          fetchSubmodules = true;
-        }
+      (builtins.fetchurl {
+        url = https://github.com/melange-re/melange/releases/download/3.0.0-v51/melange-3.0.0-v51.tbz;
+        sha256 = "1apismbgyq8m8kl1hh43wcb6ffp4hmz2ly53qdwfpmj1rb03cnp6";
+      })
     else
-    # https://github.com/melange-re/melange/tree/v3-414
-      fetchFromGitHub
-        {
-          owner = "melange-re";
-          repo = "melange";
-          rev = "794842d87f5fce7121eb8b2add65e91392c69fe3";
-          hash = "sha256-s7Lo7cYZA0BYJcNH1f2sUOxgr/eEo/Vl3U+zn6FK6/Y=";
-          fetchSubmodules = true;
-        };
+      builtins.fetchurl {
+        url = https://github.com/melange-re/melange/releases/download/3.0.0-v414/melange-3.0.0-v414.tbz;
+        sha256 = "122vxyprcfpgymz8w815z8kqaa9q1warfhzm70j5bs8v7r9jj7wl";
+      };
 
   doCheck = lib.versionOlder "5.1" ocaml.version;
 
