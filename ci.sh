f593dd08ab07057d8f945a105d9ef52efdc8d8e6
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-01-30 10:44:50 -0800

    Add a M1 runner pipeline

--- ci.sh
+++ ci.sh
@@ -5,10 +5,9 @@ args=(
   --accept-flake-config
   --check-cache-status
   --gc-roots-dir gc-root
-  --max-memory-size "12000"
+  --max-memory-size "7000"
   --option allow-import-from-derivation false
   --show-trace
-  --workers 4
   --flake ".#hydraJobs.${1}.${2}"
 )
 
