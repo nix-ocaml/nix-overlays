537cb8aa7a9f3eac5687c42172678a74ef7fd66f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-02-18 17:12:48 -0800

    upgrade dune to get virtual libs for melange, add merlin / reason
    more upgrades

--- overlay/default.nix
+++ overlay/default.nix
@@ -73,6 +73,11 @@ in
     gssSupport = false;
     openssl = self.openssl-oc;
   }).overrideAttrs (o: {
+    src = super.fetchurl {
+      url = "mirror://postgresql/source/v16.2/postgresql-16.2.tar.bz2";
+      hash = "sha256-RG6IKU28LJCFq0twYaZG+mBLS+wDUh1epnHC5a2bKVI=";
+    };
+
     doCheck = false;
     configureFlags = [
       "--without-ldap"
@@ -98,12 +103,6 @@ in
         src = "${nixpkgs}/pkgs/servers/sql/postgresql/locale-binary-path.patch";
         locale = "${if stdenv.isDarwin then super.darwin.adv_cmds else lib.getBin stdenv.cc.libc}/bin/locale";
       })
-      (super.fetchurl
-        # https://www.postgresql.org/message-id/CACpMh%2BDMZVHM%2BiDSyqdcpK8sr7jd_HxxLJRNvGTzcLBE0W07QA%40mail.gmail.com
-        {
-          url = "https://www.postgresql.org/message-id/attachment/152769/v1-0001-Make-PostgreSQL-work-with-newer-version-of-libxml.patch";
-          hash = "sha256-1j5mtG++hFmYwfS98PdN1SmNI4T86q4FXvKLz2VeJyg=";
-        })
     ] ++ lib.optionals stdenv.isLinux [
       "${nixpkgs}/pkgs/servers/sql/postgresql/patches/socketdir-in-run-13.patch"
     ];
