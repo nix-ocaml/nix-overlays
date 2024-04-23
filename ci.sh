8f4a5dbbf479a40dc70230bf945be6240172ae41
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-04-23 10:31:16 -0700

    fix: upload (#1414)

--- ci.sh
+++ ci.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 args=(
   --flake ".#hydraJobs.${1}.${2}"
-  --copy-to "s3://overlays?endpoint=https://7a53c28e9b7a91239f9ed42da04276bc.r2.cloudflarestorage.com"
+  --copy-to
+  "s3://overlays?endpoint=https://7a53c28e9b7a91239f9ed42da04276bc.r2.cloudflarestorage.com&compression=zstd&parallel-compression=true&secret-key=${HOME}/.nix/nix-cache-key.sec"
   # --no-link
   # --eval-workers 4
   # --skip-cached
@@ -13,4 +14,4 @@ args=(
   # --quiet-build
 )
 
-OCAMLRUNPARAM=b nix run 'github:nix-ocaml/nix-ci-build?rev=40961cef7eee46f6af45f369b5119cdd44e6a2d1' -- "${args[@]}"
+OCAMLRUNPARAM=b nix run github:nix-ocaml/nix-ci-build -- "${args[@]}"
