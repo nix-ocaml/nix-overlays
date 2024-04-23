74ef1c1e2cf393258bb703579cb317d6393bc8de
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-04-22 18:30:47 -0700

    try nix-ci-build

--- ci.sh
+++ ci.sh
@@ -2,24 +2,14 @@
 set -euo pipefail
 
 args=(
-  --no-link
-  --eval-workers 4
   --flake ".#hydraJobs.${1}.${2}"
-  --skip-cached
-  --no-nom
-  --option allow-import-from-derivation false
-  --eval-max-memory-size "12000"
-  --quiet-build
+  # --no-link
+  # --eval-workers 4
+  # --skip-cached
+  # --no-nom
+  # --option allow-import-from-derivation false
+  # --eval-max-memory-size "12000"
+  # --quiet-build
 )
 
-if [[ -n "${GITHUB_STEP_SUMMARY-}" ]]; then
-  log() {
-    echo "$*" >> "$GITHUB_STEP_SUMMARY"
-  }
-else
-  log() {
-    echo "$*"
-  }
-fi
-
-nix run github:anmonteiro/nix-fast-build?rev=036e4c3efb80ee17aa2d848ba0a0ef7d162a832c -- "${args[@]}"
+nix run github:anmonteiro/nix-ci-build -- "${args[@]}"
