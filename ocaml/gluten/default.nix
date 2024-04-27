1d087211e4fe2e8f2ed2bae30c3fb012860a8943
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-04-27 01:09:33 -0700

    fix hash

--- ocaml/gluten/default.nix
+++ ocaml/gluten/default.nix
@@ -8,7 +8,7 @@ buildDunePackage {
     owner = "anmonteiro";
     repo = "gluten";
     rev = "c4e50854b1a535776d85d27fdedb632852ba6519";
-    hash = "sha256-wl6fgMVz996su8+pRFzjvQa2BPDtsIo1eQ9X2REzJsc=";
+    hash = "sha256-MWUEOE4QKSuWPBC7adtdCRfzWvgswwYVa/ZcAy2c1z8=";
   };
   propagatedBuildInputs = [ bigstringaf faraday ];
 }
