f59da5e051cc753534b0329f4f8bd87f21a3972a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-06-17 21:35:33 -0700

    wip

--- ocaml/httpun/types.nix
+++ ocaml/httpun/types.nix
@@ -5,10 +5,8 @@ buildDunePackage {
   pname = "httpun-types";
   propagatedBuildInputs = [ faraday ];
 
-  src = fetchFromGitHub {
-    owner = "anmonteiro";
-    repo = "httpun";
-    rev = "c1c622617339d0ec69d5c3dd25dd72271ea4bed9";
-    hash = "sha256-Czve6io/Mh87S9i1oKMrzqH9k1Ip3WuUzMCIIKshHMo=";
+  src = builtins.fetchurl {
+    url = https://github.com/anmonteiro/httpun/releases/download/0.1.0/httpun-0.1.0.tbz;
+    sha256 = "1lclla34qc03yss3vfbw83nmxg3r9ccik6013vn8vkz189glc1sh";
   };
 }
