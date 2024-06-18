17ffb33c514935f427b7262dc7c03ee890ce85fc
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-06-17 23:22:51 -0700

    update httpun-ws to released version

--- ocaml/httpun-ws/default.nix
+++ ocaml/httpun-ws/default.nix
@@ -3,11 +3,9 @@
 buildDunePackage {
   pname = "httpun-ws";
   version = "n/a";
-  src = fetchFromGitHub {
-    owner = "anmonteiro";
-    repo = "websocketaf";
-    rev = "9418a9a192616955532498c454702275a0795643";
-    hash = "sha256-YtEMtgCBWPg6uBJDVAVMzLhOlA8LvQoEVR4NSCEI/fk=";
+  src = builtins.fetchurl {
+    url = https://github.com/anmonteiro/httpun-ws/releases/download/0.1.0/httpun-ws-0.1.0.tbz;
+    sha256 = "1p0z8sbzsm42ifl3awc923zz24f559lz5w3vn200wr0f0imcl6cx";
   };
 
   propagatedBuildInputs = [ angstrom faraday gluten httpaf base64 ];
