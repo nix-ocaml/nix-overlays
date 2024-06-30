a239d347d1f1f55698cbf91faf1983867c2bfe44
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-06-29 20:06:51 -0700

    update eio-ssl, ocamlbuild

--- ocaml/eio-ssl/default.nix
+++ ocaml/eio-ssl/default.nix
@@ -3,11 +3,9 @@
 buildDunePackage {
   pname = "eio-ssl";
   version = "n/a";
-  src = fetchFromGitHub {
-    owner = "anmonteiro";
-    repo = "eio-ssl";
-    rev = "5e83c3ffb2200affde9d900de02f6994dd6421da";
-    hash = "sha256-rkk4OA+kq2jFuKwVtR9MRDtMXt7WYc9fBB+CgY+n6Kc=";
+  src = builtins.fetchurl {
+    url = https://github.com/anmonteiro/eio-ssl/releases/download/0.3.0/eio-ssl-0.3.0.tbz;
+    sha256 = "02pjffsbyvsdi43948xqpnr8x65n3z00wnyq5qgj6map1d8s504v";
   };
   propagatedBuildInputs = [ ssl eio ];
 }
