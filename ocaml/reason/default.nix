b42e28b789f7a99454928bac1103459749d9a696
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-07-22 21:22:07 -0700

    update reason to 3.12.0

--- ocaml/reason/default.nix
+++ ocaml/reason/default.nix
@@ -15,11 +15,11 @@
 
 buildDunePackage rec {
   pname = "reason";
-  version = "3.11.0";
+  version = "3.12.0";
 
   src = builtins.fetchurl {
-    url = "https://github.com/reasonml/reason/releases/download/3.11.0/reason-3.11.0.tbz";
-    sha256 = "0l3jlahrhk18bgynqa3l6lsn9vhnxf553mcrxg44gw3r9bqkg255";
+    url = "https://github.com/reasonml/reason/releases/download/3.12.0/reason-3.12.0.tbz";
+    sha256 = "1zhskaqfy255kpsxhz01wyx12qmma0knl1sc7ld700z2zfpm1nb3";
   };
 
   propagatedBuildInputs = [
