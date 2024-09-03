cdaeed3f22ddb92d39b03c84cec40bc6c44430bb
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-09-03 16:20:43 +0200

    add ocaml 5.3 to the pipeline (#1628)
    * add ocaml 5.3 to the pipeline
    
    add jobs
    
    5.3 patch
    
    apply https://github.com/ocaml/ocaml/pull/13414
    
    use newer 5.3 hash
    
    update ocaml hash
    
    update patch
    
    update utop
    
    add merlin-extend
    
    apply https://github.com/ocaml/ocaml/pull/13414
    
    update patch
    
    * update reason
    
    * update oidc
    
    * continue on error for 5.3

--- ci/hydra.nix
+++ ci/hydra.nix
@@ -22,6 +22,11 @@ with filter;
     ocamlVersion = "5_2";
     extraIgnores = extraIgnores ++ ocaml5Ignores;
   };
+  build_5_3 = ocamlCandidates {
+    inherit pkgs;
+    ocamlVersion = "5_3";
+    extraIgnores = extraIgnores ++ ocaml5Ignores;
+  };
 
   build_top-level-packages =
     { inherit (pkgs) melange-relay-compiler hermes; } //
@@ -59,6 +64,17 @@ with filter;
 
   musl_5_2 = (if system == "x86_64-linux" then
     crossTarget pkgs.pkgsCross.musl64 "5_2"
+  else
+    { }
+  );
+
+  arm64_5_3 = (if system == "x86_64-linux" then
+    crossTarget pkgs.pkgsCross.aarch64-multiplatform-musl "5_3"
+  else
+    { });
+
+  musl_5_3 = (if system == "x86_64-linux" then
+    crossTarget pkgs.pkgsCross.musl64 "5_3"
   else
     { }
   );
