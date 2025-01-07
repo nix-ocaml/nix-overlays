02cbf6041542ccd3b4126fea80cf241ccfd95f09
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-01-06 21:58:17 -0800

    update ppxlib, ppx_rapper, pg_query, pcre

--- ocaml/pg_query/default.nix
+++ ocaml/pg_query/default.nix
@@ -11,11 +11,9 @@ buildDunePackage {
   pname = "pg_query";
   version = "0.9.7";
 
-  src = fetchFromGitHub {
-    owner = "anmonteiro";
-    repo = "pg_query-ocaml";
-    rev = "703b4dbfdf4aaee284ca53b8a92682210b0ed0a7";
-    hash = "sha256-ICOh2N6ei8d7Ar0c1YOYV/NhhOzq2uLuIKu5dzMP9tM=";
+  src = builtins.fetchurl {
+    url = "https://github.com/roddyyaga/pg_query-ocaml/releases/download/0.9.8/pg_query-0.9.8.tbz";
+    sha256 = "11c31a10g44m487anwdqfnbxxjl9jlaj277845wmv1zprhcl5lmi";
   };
 
   propagatedBuildInputs = [
