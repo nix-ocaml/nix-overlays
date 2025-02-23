ca3c30cf13eb3bac7f74e824d4c53db05901e37a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-02-23 15:29:04 -0800

    melange: update to 5.0.1

--- ocaml/melange/default.nix
+++ ocaml/melange/default.nix
@@ -20,32 +20,32 @@
 
 buildDunePackage {
   pname = "melange";
-  version = "5.0.0";
+  version = "5.0.1";
   duneVersion = "3";
 
   src =
-    if (lib.versionOlder "5.3" ocaml.version) then
+    if lib.versionOlder "5.3" ocaml.version then
       builtins.fetchurl
         {
-          url = "https://github.com/melange-re/melange/releases/download/5.0.0-53/melange-5.0.0-53.tbz";
-          sha256 = "1c2xmc471z5s97sbrvxibryl9qw8m1m922bw8gw0w9a4v16zz7b5";
+          url = "https://github.com/melange-re/melange/releases/download/5.0.1-53/melange-5.0.1-53.tbz";
+          sha256 = "1v4gvbs4sg9w0q00i7laq73zfsl184a42qhzqk3v1723wl0nxcmb";
         }
-    else if (lib.versionOlder "5.2" ocaml.version) then
+    else if lib.versionOlder "5.2" ocaml.version then
       builtins.fetchurl
         {
-          url = "https://github.com/melange-re/melange/releases/download/5.0.0-52/melange-5.0.0-52.tbz";
-          sha256 = "0r712vh5kf6cxkajn47j7zx58r9jrh8z6r7a2ngpn277rf4c2a0g";
+          url = "https://github.com/melange-re/melange/releases/download/5.0.1-52/melange-5.0.1-52.tbz";
+          sha256 = "04xnp651y13rn4chcsawnpyks3fp6wirrsl62mdhmiggv94a4qk6";
         }
-    else if (lib.versionOlder "5.1" ocaml.version) then
+    else if lib.versionOlder "5.1" ocaml.version then
       builtins.fetchurl
         {
-          url = "https://github.com/melange-re/melange/releases/download/5.0.0-51/melange-5.0.0-51.tbz";
-          sha256 = "181c2rkwindn6mnqm1p8a1x7rhf2kz0q81v9g1hqw3n4mjk3mxdc";
+          url = "https://github.com/melange-re/melange/releases/download/5.0.1-51/melange-5.0.1-51.tbz";
+          sha256 = "0vpw28qn8rvpb06dcgxgq7z0s8y7lxmwjwgc2336mp7jfindllvp";
         }
     else
       builtins.fetchurl {
-        url = "https://github.com/melange-re/melange/releases/download/5.0.0-414/melange-5.0.0-414.tbz";
-        sha256 = "1fpskdmgrhcfqqkirg56h9592ynqnri1fmcgywcdbrcv3q9svgyk";
+        url = "https://github.com/melange-re/melange/releases/download/5.0.1-414/melange-5.0.1-414.tbz";
+        sha256 = "10n2ds6b9ajq7ccazp0sffx9jnxlazlbkd38vyz0s2zg0la72vgh";
       };
 
   doCheck = false;
