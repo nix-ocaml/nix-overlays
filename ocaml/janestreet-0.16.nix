baeae17459e1150426334f7be3bfe83ec0d752c9
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-04-25 10:41:41 -0700

    upgrade base, core_unix (#1983)
    * upgrade base, core_unix
    
    * revert 0.16

--- ocaml/janestreet-0.16.nix
+++ ocaml/janestreet-0.16.nix
@@ -208,6 +208,8 @@ in
     pname = "base";
     version = "0.16.3";
     hash = "sha256-PA7nhGud/gCv22cow+KrSgNKgi2NYq/zGsCEAgVRBac=";
+    # version = "0.16.4";
+    # hash = "sha256-1kwHf3SWUNadw0J6L0X6H17Xqh8YtxpMO7Jm5Q0KoQc=";
     meta.description = "Full standard library replacement for OCaml";
     buildInputs = [ dune-configurator ];
     propagatedBuildInputs = [ sexplib0 ];
@@ -855,8 +857,8 @@ in
   ocaml_intrinsics = janePackage {
     pname = "ocaml_intrinsics";
     minimalOCamlVersion = "4.14";
-    version = "0.16.1";
-    hash = "sha256-+PJ0HiJhXSQ38CiniIVEW4msCbq/JSDpV1hpSofEfVQ=";
+    version = "0.16.2";
+    hash = "sha256-shPuLsdbLglYCSAzPFS+TC2rctWHRECQbFaWu0YTvdQ=";
     meta.description = "Intrinsics";
     buildInputs = [ dune-configurator ];
     doCheck = false; # test rules broken
