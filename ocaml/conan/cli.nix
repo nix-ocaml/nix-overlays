b9264fd43af24d12de281433556a5e5643ea89fc
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-09-19 00:13:40 -0700

    fix, rm upstreamed

--- ocaml/conan/cli.nix
+++
@@ -1,7 +0,0 @@
-{ buildDunePackage, conan, conan-database, conan-unix, dune-site }:
-
-buildDunePackage {
-  inherit (conan) version src;
-  pname = "conan-cli";
-  propagatedBuildInputs = [ conan conan-database conan-unix dune-site ];
-}
