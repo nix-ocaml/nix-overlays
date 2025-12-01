7eca5b770bfcd31b925427d9611d530185016f33
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-11-30 19:55:21 -0800

    fix confero downloads (#2213)
    unicode.org is not reliable

---
+++ ocaml/confero/default.nix
@@ -0,0 +1,42 @@
+{ buildDunePackage
+, fetchFromGitHub
+, unzip
+, angstrom
+, angstrom-unix
+, cmdliner
+, fmt
+, iso639
+, uucp
+, uunf
+, uutf
+}:
+
+buildDunePackage {
+  pname = "confero";
+  version = "0.1.1";
+  src = fetchFromGitHub {
+    owner = "paurkedal";
+    repo = "confero";
+    rev = "252cf3e";
+    sha256 = "sha256-YJyyT4uimLJQH0/bIMe/FCPk0ZYemgHYxV4uaQXVE6w=";
+  };
+
+  nativeBuildInputs = [ unzip ];
+  postPatch = ''
+    cp ${./allkeys.txt} ./lib/ducet/allkeys.txt
+    cp ${./CollationTest.zip} ./test/CollationTest.zip
+  '';
+
+  doCheck = false;
+
+  propagatedBuildInputs = [
+    angstrom
+    angstrom-unix
+    cmdliner
+    fmt
+    iso639
+    uucp
+    uunf
+    uutf
+  ];
+}
