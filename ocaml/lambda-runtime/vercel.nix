049e68346f0a92056cd290e1d01ff6cbbcb32e82
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-07-05 17:06:40 -0700

    fix: vercel

--- ocaml/lambda-runtime/vercel.nix
+++ ocaml/lambda-runtime/vercel.nix
@@ -4,6 +4,7 @@
   piaf,
   yojson,
   ppx_deriving_yojson,
+  ppxlib_gt_0_37,
   lwt,
   base64,
 
@@ -12,6 +13,7 @@
 buildDunePackage {
   pname = "vercel";
   inherit (lambda-runtime) version src;
+  buildInputs = [ ppxlib_gt_0_37 ];
   propagatedBuildInputs = [
     lambda-runtime
     piaf
