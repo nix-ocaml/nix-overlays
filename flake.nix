80bc3de4975e1b879156ef9a0b1ae2bc6167de96
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-07-11 15:43:56 -0700

    Drop x86_64-darwin hydra jobs

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     {
       lib = nixpkgs.lib;
 
-      hydraJobs = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (
+      hydraJobs = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (
         system:
         import ./ci/hydra.nix {
           inherit system;
