e487dc3b52bee59298c46594711a3995705171cd
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-07-25 13:45:36 -0700

    update eio / eio-ssl / ppx_deriving (#2544)
    * update eio / eio-ssl / ppx_deriving
    
    * uring
    
    * fix eio 1.4 build dependencies
    
    * restore eio-ssl version guard
    
    * update piaf for eio 1.4
    
    * gate eio 1.4 on OCaml 5.2
    
    * fix uring 2.15 on musl
    
    * fix piaf package compatibility

--- ocaml/eio-ssl/default.nix
+++ ocaml/eio-ssl/default.nix
@@ -8,9 +8,11 @@
 buildDunePackage {
   pname = "eio-ssl";
   version = "n/a";
-  src = builtins.fetchurl {
-    url = "https://github.com/anmonteiro/eio-ssl/releases/download/0.3.0/eio-ssl-0.3.0.tbz";
-    sha256 = "02pjffsbyvsdi43948xqpnr8x65n3z00wnyq5qgj6map1d8s504v";
+  src = fetchFromGitHub {
+    owner = "anmonteiro";
+    repo = "eio-ssl";
+    rev = "0b8560cd6e18ca248af10fb23fe31a8e0068fff6";
+    hash = "sha256-XhayIlGwGJm093W6DtILoEFfdhXomR44+mvz5Q9Yr1k=";
   };
   propagatedBuildInputs = [
     ssl
