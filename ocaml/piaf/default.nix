8119b94e52d6c17d672ec1ae480cf5607075dbbd
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-07-25 13:39:49 -0700

    fix piaf package compatibility

--- ocaml/piaf/default.nix
+++ ocaml/piaf/default.nix
@@ -4,6 +4,8 @@
   fetchFromGitHub,
   buildDunePackage,
   dune-configurator,
+  pkg-config,
+  openssl-oc,
   eio,
   eio_main,
   eio-ssl,
@@ -22,19 +24,35 @@
   dune-site,
   ocaml,
 }:
-
+let
+  hasEio14 = lib.versionAtLeast ocaml.version "5.2";
+in
 buildDunePackage {
   pname = "piaf";
   version = "n/a";
-  src = fetchFromGitHub {
-    owner = "anmonteiro";
-    repo = "piaf";
-    rev = "b8f5e94deea6e653025c37131bbf6833dc4c4c85";
-    hash = "sha256-XnyJXkp0WCUsZO67rUNMjEdhZZgAJAUOfxha7V2OvuI=";
-    fetchSubmodules = true;
-  };
+  src =
+    if hasEio14 then
+      fetchFromGitHub {
+        owner = "anmonteiro";
+        repo = "piaf";
+        rev = "b8f5e94deea6e653025c37131bbf6833dc4c4c85";
+        hash = "sha256-XnyJXkp0WCUsZO67rUNMjEdhZZgAJAUOfxha7V2OvuI=";
+        fetchSubmodules = true;
+      }
+    else
+      fetchFromGitHub {
+        owner = "anmonteiro";
+        repo = "piaf";
+        rev = "c27b38e4493e81b0a7c895c620976bf155f14d8b";
+        hash = "sha256-3IUxX3Zax5ddMTKJufMCeRvZoFWbTYMvfHFFpN15PuA=";
+        fetchSubmodules = true;
+      };
 
-  buildInputs = [ dune-configurator ];
+  nativeBuildInputs = lib.optionals hasEio14 [ pkg-config ];
+  buildInputs = lib.optionals hasEio14 [
+    dune-configurator
+    openssl-oc
+  ];
 
   propagatedBuildInputs = [
     eio
