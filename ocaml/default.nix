11e95905c285ea906963d58070fe866800e26ad0
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-08-18 18:13:13 -0700

    Update sources (#2619)
    * Update sources
    
    * rm upstreamed
    
    ---------
    
    Co-authored-by: UpdateBot <ulrikstrid@users.noreply.github.com>

--- ocaml/default.nix
+++ ocaml/default.nix
@@ -1815,13 +1815,6 @@ with oself;
     ];
   };
 
-  mirage-logs = osuper.mirage-logs.overrideAttrs (_: {
-    src = builtins.fetchurl {
-      url = "https://github.com/mirage/mirage-logs/releases/download/v2.1.0/mirage-logs-2.1.0.tbz";
-      sha256 = "1fww8q0an84wiqfwycqlv9chc52a9apf6swbiqk28h1v1jrc52mf";
-    };
-  });
-
   msat = osuper.msat.overrideAttrs (o: {
     postPatch = (o.postPatch or "") + ''
       substituteInPlace dune \
