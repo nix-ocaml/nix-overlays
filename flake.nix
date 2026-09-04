d0e5e038516bf23da98804e8aa90c2b3b57b830f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-09-04 15:57:45 -0700

    Update sources (#2651)
    Co-authored-by: UpdateBot <ulrikstrid@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=074b06a122e9f3e245e17fdc1199c3481c8b70b5";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=8a37cfb926b31472f2e90b992c197c62d6bb4d74";
   };
 
   outputs =
